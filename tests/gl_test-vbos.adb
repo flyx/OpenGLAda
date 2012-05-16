1318b6c5d6a3abc4af0db87dfeb201285b8f44e6
Felix Krause <flyx@isobeef.org>
2012-05-16 19:27:33 +0200

    Modified test to count FPS

--- tests/gl_test-vbos.adb
+++ tests/gl_test-vbos.adb
@@ -26,6 +26,7 @@ with GL.Fixed.Matrix;
 with GL.Fixed.Immediate;
 with GL.Types;
 
+with Ada.Calendar;
 with Ada.Text_IO;
 
 procedure GL_Test.VBOs is
@@ -78,6 +79,10 @@ begin
       Index_Buffer : Buffer_Object;
       
       Rotator : Double := 0.0;
+      
+      Cur_Seconds  : Ada.Calendar.Day_Duration;
+      Last_Seconds : Ada.Calendar.Day_Duration := 0.0;
+      Frame_Count  : Natural;
    begin
       Array_Buffer.Bind (Cube_Buffer);
       Load_Vector (Array_Buffer, Cube, Static_Draw);
@@ -107,7 +112,14 @@ begin
          
          Glfw.Display.Swap_Buffers;
          
-         delay 0.1;
+         Cur_Seconds := Ada.Calendar.Seconds (Ada.Calendar.Clock);
+         Frame_Count := Frame_Count + 1;
+         
+         if Cur_Seconds > Last_Seconds + 1.0 or Cur_Seconds < Last_Seconds then
+            Last_Seconds := Cur_Seconds;
+            Glfw.Display.Set_Title ("FPS:" & Frame_Count'Img);
+            Frame_Count := 0;
+         end if;
          
          Glfw.Events.Poll_Events;
       end loop;
