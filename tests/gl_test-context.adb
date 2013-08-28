170e44abe8ee335a2bcb3bf44be973b164f1053c
Felix Krause <flyx@isobeef.org>
2013-08-28 19:49:20 +0200

    Removed obsolete src dir & extended context test

--- tests/gl_test-context.adb
+++ tests/gl_test-context.adb
@@ -30,7 +30,8 @@ procedure GL_Test.Context is
       begin
          Put_Line ("Major version: " & GL.Context.Major_Version'Img);
          Put_Line ("Minor version: " & GL.Context.Minor_Version'Img);
-      exception when others =>
+      exception
+         when others =>
             Put_Line ("OpenGL version too old for querying major and minor");
       end;
       Put_Line ("Whole version string: " & GL.Context.Version_String);
@@ -46,16 +47,25 @@ procedure GL_Test.Context is
             begin
                Put_Line ("  " & Name);
                if not GL.Context.Has_Extension (Name) then
-                  Put_Line ("!!! Has_Extension returns false (should not happen)");
+                  Put_Line
+                    ("!!! Has_Extension returns false (should not happen)");
                end if;
             end;
          end loop;
       end;
-      Put_Line ("Primary shading language version: " &
-                  GL.Context.Primary_Shading_Language_Version);
+      begin
+         Put_Line ("Primary shading language version: " &
+                   GL.Context.Primary_Shading_Language_Version);
+      exception
+         when others =>
+            Put_Line ("OpenGL version too old for querying " &
+                      "Primary shading language version");
+            return;
+      end;
       begin
          declare
-            List : GL.Context.String_List := GL.Context.Supported_Shading_Language_Versions;
+            List : GL.Context.String_List :=
+              GL.Context.Supported_Shading_Language_Versions;
          begin
             Put_Line ("Supported shading language versions:");
             for I in List'Range loop
@@ -63,38 +73,58 @@ procedure GL_Test.Context is
                   Version : String := To_String (List (I));
                begin
                   Put_Line ("  " & Version);
-                  if not GL.Context.Supports_Shading_Language_Version (Version) then
-                     Put_Line ("!!! Supports_Shading_Language_Version returns false (should not happen)");
+                  if not GL.Context.Supports_Shading_Language_Version
+                    (Version)
+                  then
+                     Put_Line ("!!! Supports_Shading_Language_Version " &
+                               "returns false (should not happen)");
                   end if;
                end;
             end loop;
          end;
-      exception when others =>
+      exception
+         when others =>
             Put_Line ("OpenGL version too old for querying supported versions");
       end;
    end Display_Context_Information;
 
 begin
    Glfw.Init;
+   
+   Ada.Text_IO.Put_Line
+     ("Getting information without request for OpenGL version.");
+   
+   -- create a window so we have a context
+   Glfw.Display.Open (Mode   => Glfw.Display.Window,
+                      Width  => 500,
+                      Height => 500);
+ 
+   Display_Context_Information;
+   Ada.Text_IO.New_Line;
+ 
+   Glfw.Display.Close;
 
-   Ada.Text_IO.Put_Line ("Getting information with requested OpenGL version 2.1:");
+   Ada.Text_IO.Put_Line
+     ("Getting information with requested OpenGL version 2.1:");
 
    Glfw.Display.Hint_Minimum_OpenGL_Version (Major => 2, Minor => 1);
 
-   -- create a window so we have a context
-   Glfw.Display.Open (Mode  => Glfw.Display.Window,
-                      Width => 500, Height => 500);
+   Glfw.Display.Open (Mode   => Glfw.Display.Window,
+                      Width  => 500,
+                      Height => 500);
 
    Display_Context_Information;
 
    Glfw.Display.Close;
 
-   Ada.Text_IO.Put_Line ("Getting information with requested OpenGL version 3.2:");
+   Ada.Text_IO.Put_Line
+     ("Getting information with requested OpenGL version 3.2:");
 
    Glfw.Display.Hint_Minimum_OpenGL_Version (Major => 3, Minor => 2);
 
-   Glfw.Display.Open (Mode  => Glfw.Display.Window,
-                      Width => 500, Height => 500);
+   Glfw.Display.Open (Mode   => Glfw.Display.Window,
+                      Width  => 500,
+                      Height => 500);
 
    Display_Context_Information;
 
