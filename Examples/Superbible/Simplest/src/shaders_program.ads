bb2a0997573e116409a528c773c0c80403ca0a48
Roger <Roger@Rogers-Mac-mini.local>
2016-12-28 12:23:45 +1100

    Style Corrections

--- Examples/Superbible/Simplest/src/shaders_program.ads
+++ Examples/Superbible/Simplest/src/shaders_program.ads
@@ -5,7 +5,7 @@ with Window_Types;
 
 package Shaders_Program is
 
-    function Make_Shader_Program(Window         : Window_Types.tWindow;
-                                 Shader_Program : out GL.Objects.Programs.Program) return Boolean;
+    function Make_Shader_Program (Window         : Window_Types.tWindow;
+                                  Shader_Program : out GL.Objects.Programs.Program) return Boolean;
 
 end Shaders_Program;
