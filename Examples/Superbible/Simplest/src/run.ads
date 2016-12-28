61c734468d466bfa7d77d5228a04e8689dd38f18
Roger <Roger@Rogers-Mac-mini.local>
2016-12-28 12:23:01 +1100

    Style Corrections

--- Examples/Superbible/Simplest/src/run.ads
+++ Examples/Superbible/Simplest/src/run.ads
@@ -4,6 +4,6 @@ with GL.Objects.Programs;
 with Window_Types;
 
 package Run is
-    procedure Main_Loop(Main_Window    : in out Window_Types.tWindow;
-                                         Rendering_Program   : GL.Objects.Programs.Program);
+    procedure Main_Loop (Main_Window         : in out Window_Types.tWindow;
+                         Rendering_Program   : GL.Objects.Programs.Program);
 end Run;
