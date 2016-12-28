7f89fd94e13c224699336b7b7114ccb7e0b5891a
Roger <Roger@Rogers-Mac-mini.local>
2016-12-28 17:22:31 +1100

    run.ads changed to match run.adb

--- Examples/Superbible/Simplest/src/run.ads
+++ Examples/Superbible/Simplest/src/run.ads
@@ -1,9 +1,6 @@
 
-with GL.Objects.Programs;
-
 with Window_Types;
 
 package Run is
-    procedure Main_Loop (Main_Window         : in out Window_Types.tWindow;
-                         Rendering_Program   : GL.Objects.Programs.Program);
+    procedure Main_Loop (Main_Window : in out Window_Types.tWindow);
 end Run;
