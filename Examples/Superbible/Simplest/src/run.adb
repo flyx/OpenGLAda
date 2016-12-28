3c688c779391ad853596ca93772f515d4d2f0500
Roger <Roger@Rogers-Mac-mini.local>
2016-12-28 12:22:54 +1100

    Style Corrections

--- Examples/Superbible/Simplest/src/run.adb
+++ Examples/Superbible/Simplest/src/run.adb
@@ -7,16 +7,16 @@ with Render;
 
 package body Run is
 
-    procedure Main_Loop(Main_Window         :  in out Window_Types.tWindow;
-                        Rendering_Program   : GL.Objects.Programs.Program) is
+    procedure Main_Loop (Main_Window         :  in out Window_Types.tWindow;
+                         Rendering_Program   : GL.Objects.Programs.Program) is
         use Glfw.Input;
         Running  : Boolean := True;
     begin
         while Running loop
-            Render.Render(Rendering_Program, glfw.Time);
-            glfw.Windows.Context.Swap_Buffers(Main_Window'Access);
+            Render.Render (Rendering_Program, glfw.Time);
+            glfw.Windows.Context.Swap_Buffers (Main_Window'Access);
             glfw.Input.Poll_Events;
-            Running := Running and not (Main_Window.Key_State(glfw.Input.Keys.Escape) = Glfw.Input.Pressed);
+            Running := Running and not (Main_Window.Key_State (glfw.Input.Keys.Escape) = Glfw.Input.Pressed);
             Running := Running and not Main_Window.Should_Close;
         end loop;
     end Main_Loop;
