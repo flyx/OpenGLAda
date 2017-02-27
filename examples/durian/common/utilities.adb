04e82fe6b8ab6558f5f1b7535f0a95dc5639afa0
Roger <rogermc@grapevine.com.au>
2017-02-28 05:30:17 +1100

    Durian Textures Example

--- examples/durian/common/utilities.adb
+++ examples/durian/common/utilities.adb
@@ -4,25 +4,25 @@ with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Strings.Unbounded; use  Ada.Strings.Unbounded;
 
 with GL.Buffers;
-with GL.Context;
+with gl.Context;
 with GL.Errors;
 with GL.Objects.Shaders.Lists;
-with GL.Types; use GL.Types;
+with GL.Types.Colors;
 
 package body Utilities is
 
    procedure Clear_Background_Colour (Colour : GL.Types.Colors.Color) is
    begin
-        GL.Buffers.Set_Color_Clear_Value (Colour);
         GL.Buffers.Clear ((False, False, False, True));
+        GL.Buffers.Set_Color_Clear_Value (Colour);
    end Clear_Background_Colour;
 
     --  ------------------------------------------------------------------------
 
    procedure Clear_Background_Colour_And_Depth (Colour : GL.Types.Colors.Color) is
    begin
-        GL.Buffers.Set_Color_Clear_Value (Colour);
         GL.Buffers.Clear ((True, False, False, True));
+        GL.Buffers.Set_Color_Clear_Value (Colour);
    end Clear_Background_Colour_And_Depth;
 
     --  ------------------------------------------------------------------------
