6feb1ba74796fb3d5ecfde6cbb7c59c26f5fcab1
Roger <rogermc@grapevine.com.au>
2017-03-16 18:40:11 +1100

    Ongoing debugging.

--- examples/ogl_tutorials/tutorial04_coloured_cube/src/shaders/Transform_Vertex_Shader.glsl
+++ examples/ogl_tutorials/tutorial04_coloured_cube/src/shaders/Transform_Vertex_Shader.glsl
@@ -12,7 +12,7 @@ uniform mat4 MVP;
 
 void main()
 {
-	gl_Position =  MVP * vec4(vertexPosition_modelspace,1);
+	gl_Position =  MVP * vec4(vertexPosition_modelspace, 1.0);
 	fragment_colour = vertex_colour;
 }
 
