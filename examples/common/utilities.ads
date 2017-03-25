3e28aa6f389fb4fa1d4dc8b1ae84828f2d297fb2
Roger <rogermc@grapevine.com.au>
2017-03-25 20:11:36 +1100

    Element buffers example

--- examples/common/utilities.ads
+++ examples/common/utilities.ads
@@ -7,6 +7,8 @@ package Utilities is
 
     procedure Clear_Background_Colour (Colour : GL.Types.Colors.Color);
     procedure Clear_Background_Colour_And_Depth (Colour : GL.Types.Colors.Color);
+    procedure Print_Matrix (Name : String; aMatrix : GL.Types.Singles.Matrix3);
+    procedure Print_Matrix (Name : String; aMatrix : GL.Types.Singles.Matrix4);
     procedure Print_Vector (Name : String; aVector : GL.Types.Singles.Vector3);
     procedure Show_Shader_Program_Data (aProgram : gl.Objects.Programs.Program);
     procedure Show_GL_Data;
