e4fb9290736ac337dd8e67a3084319615bc9d087
Roger <rogermc@grapevine.com.au>
2017-03-25 16:01:20 +1100

    Use GL.Types.Single_Pointers and GL.Types.Int_Pointers.

--- examples/open_gl/coloured_elements/src/vertex_data.ads
+++ examples/open_gl/coloured_elements/src/vertex_data.ads
@@ -1,31 +1,19 @@
 
-with Interfaces.C.Pointers;
-
 with GL.Objects.Buffers;
 with GL.Types;
 
 package Vertex_Data is
     use GL.Types;
-    type tPoints_Array is array (GL.Types.Int range <>) of aliased GL.Types.Single;
-    type tElements_Array is array (GL.Types.Int range <>) of aliased GL.Types.Int;
-
-    package pVertex_Pointers is new Interfaces.C.Pointers
-      (GL.Types.Int, GL.Types.Single, tPoints_Array, 0.0);
-    package pElement_Pointers is new Interfaces.C.Pointers
-      (GL.Types.Int, GL.Types.Int, tElements_Array, 0);
 
     procedure Load_Vertex_Buffer is new
-      GL.Objects.Buffers.Load_To_Buffer (pVertex_Pointers);
+      GL.Objects.Buffers.Load_To_Buffer (GL.Types.Single_Pointers);
     procedure Load_Element_Buffer is new
-      GL.Objects.Buffers.Load_To_Buffer (pElement_Pointers);
+      GL.Objects.Buffers.Load_To_Buffer (GL.Types.Int_Pointers);
 
-    Vertices_Coloured  : tPoints_Array (0 .. 3 * 5 - 1) :=
+    Vertices_Coloured  : Single_Array (0 .. 3 * 5 - 1) :=
                       (0.0, 0.5, 1.0, 0.0, 0.0,
                        0.5, -0.5, 0.0, 1.0, 0.0,
                        -0.5, -0.5, 0.0, 0.0, 1.0);
-    Vertices_No_Colour  : tPoints_Array (0 .. 3 * 2 - 1) :=
-                      (0.0, 0.5,
-                       0.5, -0.5,
-                       -0.5, -0.5);
-    Elements  : tElements_Array (0 .. 2) := (0, 1, 2);
+
+    Elements  : Int_Array (0 .. 2) := (0, 1, 2);
 end Vertex_Data;
