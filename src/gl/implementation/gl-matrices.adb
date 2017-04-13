5ac086b2bd67a8ca878b68f0e1a7f86e8b186d8f
Felix Krause <contact@flyx.org>
2017-04-13 22:27:18 +0200

    Matrices are now column-major
     * Fixes #59

--- src/gl/implementation/gl-matrices.adb
+++ src/gl/implementation/gl-matrices.adb
@@ -14,97 +14,99 @@
 -- OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 --------------------------------------------------------------------------------
 
-package body GL.Matrices is      
+package body GL.Matrices is
    function "+" (Left, Right : Matrix) return Matrix is
       Return_Matrix : Matrix;
    begin
-      for I in Index_Type loop
-         for J in Index_Type loop
-            Return_Matrix (I, J) := Left (I, J) + Right (I, J);
+      for Column in Index_Type loop
+         for Row in Index_Type loop
+            Return_Matrix (Column, Row) :=
+              Left (Column, Row) + Right (Column, Row);
          end loop;
       end loop;
       return Return_Matrix;
    end "+";
-   
+
    function "-" (Left, Right : Matrix) return Matrix is
       Return_Matrix : Matrix;
    begin
-      for I in Index_Type loop
-         for J in Index_Type loop
-            Return_Matrix (I, J) := Left (I, J) - Right (I, J);
+      for Column in Index_Type loop
+         for Row in Index_Type loop
+            Return_Matrix (Column, Row) :=
+              Left (Column, Row) - Right (Column, Row);
          end loop;
       end loop;
       return Return_Matrix;
    end "-";
-   
+
    function "-" (Left : Matrix) return Matrix is
       Ret : Matrix;
    begin
-      for I in Index_Type loop
-         for J in Index_Type loop
-            Ret (I, J) := -Left (I, J);
+      for Column in Index_Type loop
+         for Row in Index_Type loop
+            Ret (Column, Row) := -Left (Column, Row);
          end loop;
       end loop;
       return Ret;
    end "-";
-   
+
    function "*" (Left, Right : Matrix) return Matrix is
       Element : Element_Type;
       Return_Matrix : Matrix;
    begin
-      for I in Index_Type loop
-         for J in Index_Type loop
+      for Column in Index_Type loop
+         for Row in Index_Type loop
             for X in Index_Type loop
                if X = Index_Type'First then
-                  Element := Left (I, X) * Right (X, J);
+                  Element := Left (Column, X) * Right (X, Row);
                else
-                  Element := Element + Left (I, X) * Right (X, J);
+                  Element := Element + Left (Column, X) * Right (X, Row);
                end if;
             end loop;
-            Return_Matrix (I, J) := Element;
+            Return_Matrix (Column, Row) := Element;
          end loop;
       end loop;
       return Return_Matrix;
    end "*";
-   
+
    function "*" (Left : Matrix; Right : Vector_Type) return Vector_Type is
       Return_Vector : Vector_Type;
    begin
-      for I in Index_Type loop
-         for J in Index_Type loop
-            if J = Index_Type'First then
-               Return_Vector (I) := Left (I, J) * Right (J);
+      for Row in Index_Type loop
+         for Column in Index_Type loop
+            if Row = Index_Type'First then
+               Return_Vector (Row) := Left (Column, Row) * Right (Column);
             else
-               Return_Vector (I)
-                 := Return_Vector (I) + Left (I, J) * Right (J);
+               Return_Vector (Row)
+                 := Return_Vector (Row) + Left (Column, Row) * Right (Column);
             end if;
          end loop;
       end loop;
       return Return_Vector;
    end "*";
-   
+
    function "*" (Left : Matrix; Right : Element_Type) return Matrix is
       Return_Matrix : Matrix;
    begin
-      for I in Index_Type loop
-         for J in Index_Type loop
-            Return_Matrix (I, J) := Left (I, J) * Right;
+      for Column in Index_Type loop
+         for Row in Index_Type loop
+            Return_Matrix (Column, Row) := Left (Column, Row) * Right;
          end loop;
       end loop;
       return Return_Matrix;
    end "*";
-   
+
    function "*" (Left : Element_Type; Right : Matrix) return Matrix is
    begin
       return Right * Left;
    end "*";
-   
+
    function Transpose (Subject : Matrix) return Matrix is
       Ret : Matrix;
    begin
-      for I in Index_Type loop
-         for J in Index_Type loop
-            Ret (I, J) := Subject (J, I);
+      for Column in Index_Type loop
+         for Row in Index_Type loop
+            Ret (Column, Row) := Subject (Row, Column);
          end loop;
       end loop;
       return Ret;
--- src/gl/interface/gl-matrices.ads
+++ src/gl/interface/gl-matrices.ads
@@ -26,27 +26,30 @@ generic
    type Vector_Type is array (Index_Type) of aliased Element_Type;
 package GL.Matrices is
    pragma Preelaborate;
-   
+
+   -- this matrix is column-major (i.e. the first index defines the column,
+   -- the second index defines the row).
+   -- this is important for interoperability with GLSL.
    type Matrix is array (Index_Type, Index_Type) of aliased Element_Type;
-   
+
    function "+" (Left, Right : Matrix) return Matrix;
    function "-" (Left, Right : Matrix) return Matrix;
    function "-" (Left : Matrix) return Matrix;
-   
+
    -- This is not element-wise but mathematical matrix multiplication.
    function "*" (Left, Right : Matrix) return Matrix;
-   
+
    function "*" (Left : Matrix; Right : Vector_Type) return Vector_Type;
-   
+
    function "*" (Left : Matrix; Right : Element_Type) return Matrix;
    function "*" (Left : Element_Type; Right : Matrix) return Matrix;
-   
+
    function Transpose (Subject : Matrix) return Matrix;
-   
+
    pragma Inline ("+");
    pragma Inline ("-");
    pragma Inline ("*");
    pragma Inline (Transpose);
-   
+
    pragma Convention (C, Matrix);
 end GL.Matrices;
