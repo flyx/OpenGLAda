779aba9992a32161d3477f74089be68db7057e3b
Roger <rogermc@grapevine.com.au>
2017-04-20 16:05:00 +1000

    Resolve conflicts.

--- examples/common/utilities.ads
+++ examples/common/utilities.ads
@@ -9,7 +9,6 @@ package Utilities is
     procedure Clear_All (Colour : GL.Types.Colors.Color);
     procedure Clear_Background_Colour (Colour : GL.Types.Colors.Color);
     procedure Clear_Background_Colour_And_Depth (Colour : GL.Types.Colors.Color);
-<<<<<<< HEAD
     procedure Load_UV_Buffer is new
       GL.Objects.Buffers.Load_To_Buffer (GL.Types.Singles.Vector2_Pointers);
     procedure Load_Vertex_Buffer is new
@@ -18,8 +17,6 @@ package Utilities is
       GL.Objects.Buffers.Load_To_Buffer (GL.Types.Singles.Vector4_Pointers);
     procedure Print_GL_Array3 (Name : String; anArray : GL.Types.Singles.Vector3_Array);
     procedure Print_GL_Array4 (Name : String; anArray : GL.Types.Singles.Vector4_Array);
-=======
->>>>>>> Add print matrix procedures to Utilities
     procedure Print_Matrix (Name : String; aMatrix : GL.Types.Singles.Matrix3);
     procedure Print_Matrix (Name : String; aMatrix : GL.Types.Singles.Matrix4);
     procedure Print_Vector (Name : String; aVector : GL.Types.Singles.Vector3);
