af31f393903646ffdcd67a1fd61274c7b7e8663a
Roger <rogermc@grapevine.com.au>
2017-04-22 02:56:11 +1000

    Delete replaced files.

--- examples/common/dds_loader.ads
+++
@@ -1,7 +0,0 @@
-
-with GL.Objects.Textures;
-
-package DDS_Loader is
-    procedure Load_DDS (File_Name  : String;
-                        theTexture : out GL.Objects.Textures.Texture);
-end DDS_Loader;
