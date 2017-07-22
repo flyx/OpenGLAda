37658c9fa3ef675de990f4ab92c8d09ec70448d8
Roger <rogermc@grapevine.com.au>
2017-07-22 21:49:12 +1000

    Move text setup to separate package.

---
+++ examples/ftgl/text_rendering/src/ftgl_interface.adb
@@ -0,0 +1,186 @@
+
+with Ada.Containers.Ordered_Maps;
+with Ada.Exceptions; use Ada.Exceptions;
+with Ada.Text_IO; use Ada.Text_IO;
+
+with GL.Pixels;
+with GL.Objects.Textures.Targets;
+
+with FTGL;
+
+package body FTGL_Interface is
+
+   package Characters_Package is new Ada.Containers.Ordered_Maps (Character, Character_Data);
+   type Chars_Map_Type is new Characters_Package.Map with null record;
+   type Chars_Cursor is new Characters_Package.Cursor;
+
+   --  ------------------------------------------------------------------------
+
+   function Char_Advance (Data : Character_Data) return GL.Types.Single is
+   begin
+         return Data.Advance;
+   end Char_Advance;
+
+   --  ------------------------------------------------------------------------
+
+   function Char_Ascend (Data : Character_Data)  return GL.Types.Single is
+   begin
+         return Data.Ascend;
+   end Char_Ascend;
+
+   --  ------------------------------------------------------------------------
+
+   function Char_Bearing_X (Data : Character_Data) return GL.Types.Single is
+   begin
+         return Data.Bearing_X;
+   end Char_Bearing_X;
+
+   --  ------------------------------------------------------------------------
+
+   function Char_Bearing_Y (Data : Character_Data) return GL.Types.Single is
+   begin
+         return Data.Bearing_Y;
+   end Char_Bearing_Y;
+
+   --  ------------------------------------------------------------------------
+
+   function Char_Descend (Data : Character_Data) return  GL.Types.Single is
+   begin
+         return Data.Descend;
+   end Char_Descend;
+
+   --  ------------------------------------------------------------------------
+
+   function Char_Height (Data : Character_Data) return GL.Types.Single is
+   begin
+         return Data.Height;
+   end Char_Height;
+
+   --  ------------------------------------------------------------------------
+
+   function Char_Texture (Data : Character_Data) return GL.Objects.Textures.Texture is
+   begin
+         return Data.Texture;
+   end Char_Texture;
+
+   --  ------------------------------------------------------------------------
+
+   function Char_Valid (Data : Character_Data) return Boolean is
+   begin
+         return Data.Valid;
+   end Char_Valid;
+
+   --  ------------------------------------------------------------------------
+
+   function Char_Width (Data : Character_Data) return GL.Types.Single is
+   begin
+         return Data.Width;
+   end Char_Width;
+
+   --  ------------------------------------------------------------------------
+
+   package body Setup is
+
+      procedure Generate_Texture (Font_Texture  : in out GL.Objects.Textures.Texture;
+                                  Image_Address : GL.Objects.Textures.Image_Source;
+                                  Face_Size     : GL.Types.Int) is
+         use GL.Objects.Textures;
+         use GL.Objects.Textures.Targets;
+         use GL.Pixels;
+
+      begin
+         Font_Texture.Initialize;
+         Texture_2D.Bind (Font_Texture);
+         Texture_2D.Load_From_Data  (0, RGB, Face_Size, Face_Size, Red,
+                                     Unsigned_Byte, Image_Address);
+         Texture_2D.Set_X_Wrapping (GL.Objects.Textures.Clamp_To_Edge);
+         Texture_2D.Set_Y_Wrapping (GL.Objects.Textures.Clamp_To_Edge);
+         Texture_2D.Set_Minifying_Filter (GL.Objects.Textures.Linear);
+         Texture_2D.Set_Magnifying_Filter (GL.Objects.Textures.Linear);
+
+      exception
+         when anError : FTGL.FTGL_Error =>
+            Put_Line ("Generate_Texture returned an FTGL error: ");
+            raise;
+         when  others =>
+            Put_Line ("An exception occurred in Generate_Texture.");
+            raise;
+      end Generate_Texture;
+
+      --  ------------------------------------------------------------------------
+
+      procedure Load_Characters (theFont   : in out Font_Type;
+                                 Data      : out Character_Data;
+                                 Face_Size : GL.Types.Int;
+                                 Scale     : GL.Types.Single) is
+         use FTGL.Fonts;
+         use GL.Objects.Textures;
+         use GL.Types;
+         Char_Map_List   : FTGL.Fonts.Charset_List := theFont.Get_Char_Map_List;
+         Font_Char_Map   : FTGL.Charset := Char_Map_List (5);
+         aChar           : Character;
+         Char_S          : String := " ";
+         Char_Image_Map  : Chars_Map_Type;
+         Font_Texture    : GL.Objects.Textures.Texture;
+         Image_Address   : GL.Objects.Textures.Image_Source;
+
+         --  BBox array elements:
+         --  1 lower  2 left  3 near
+         --  4 upper  5 right 6 far
+         BBox            : FTGL.Bounding_Box;
+      begin
+         for Index in 0 .. 128 loop
+            aChar := Character'Val (Index);
+            Char_S (1) := aChar;
+            BBox := theFont.Bounds (Char_S);
+            Data.Ascend := theFont.Ascender;
+            Data.Descend := theFont.Descender;
+            Data.Advance := theFont.Advance_Width (Char_S);
+            Data.Height := (BBox (4) - BBox (1)) * Scale;
+            Data.Width  := (BBox (5) - BBox (2)) * Scale;
+            Data.Bearing_X := 0.5 * Data.Width;
+            Data.Bearing_Y := Data.Height - Data.Descend;
+            Image_Address := Image_Source (Font_Char_Map'Address);
+            Generate_Texture (Font_Texture, Image_Address, Face_Size);
+            Char_Image_Map.Insert (aChar, Data);
+            Data.Texture := Font_Texture;
+            Data.Valid := True;
+         end loop;
+
+      exception
+         when anError : FTGL.FTGL_Error =>
+            Put_Line ("Generate_Texture returned an FTGL error: ");
+            raise;
+         when  others =>
+            Put_Line ("An exception occurred in Load_Char_Vector.");
+            raise;
+      end Load_Characters;
+
+      --  ------------------------------------------------------------------------
+
+      procedure Setup_Font (theFont    : in out Font_Type;
+                            Data       : out Character_Data;
+                            Font_File  : String; Face_Size : GL.Types.UInt := 72;
+                            Scale      : GL.Types.Single := 1.0) is
+      begin
+         theFont.Initialize;
+         theFont.Load (Font_File);
+         if not theFont.Loaded then
+            Put_Line (Font_File & " failed to load.");
+            raise FTGL.FTGL_Error;
+         end if;
+         Load_Characters (theFont, Data, GL.Types.Int (Face_Size), Scale);
+      exception
+         when anError : FTGL.FTGL_Error =>
+            Put_Line ("Setup_Font returned an FTGL error: ");
+            raise;
+         when  others =>
+            Put_Line ("An exception occurred in Setup_Font.");
+            raise;
+      end Setup_Font;
+
+      --  ------------------------------------------------------------------------
+
+   end Setup;
+
+end FTGL_Interface;
---
+++ examples/ftgl/text_rendering/src/ftgl_interface.ads
@@ -0,0 +1,43 @@
+
+with GL.Objects.Textures;
+with GL.Types;
+
+with FTGL.Fonts;
+
+package FTGL_Interface is
+
+   type Character_Data is private;
+
+   generic
+      type Font_Type is new FTGL.Fonts.Font with private;
+   package Setup is
+      procedure Setup_Font (theFont    : in out Font_Type;
+                            Data       : out Character_Data;
+                            Font_File  : String; Face_Size : GL.Types.UInt := 72;
+                            Scale      : GL.Types.Single := 1.0);
+   end Setup;
+
+   function Char_Advance (Data : Character_Data) return GL.Types.Single;
+   function Char_Ascend (Data : Character_Data)  return GL.Types.Single;
+   function Char_Bearing_X (Data : Character_Data) return GL.Types.Single;
+   function Char_Bearing_Y (Data : Character_Data) return GL.Types.Single;
+   function Char_Descend (Data : Character_Data) return  GL.Types.Single;
+   function Char_Height (Data : Character_Data) return GL.Types.Single;
+   function Char_Texture (Data : Character_Data) return GL.Objects.Textures.Texture;
+   function Char_Valid (Data : Character_Data) return Boolean;
+   function Char_Width (Data : Character_Data) return GL.Types.Single;
+
+private
+   type Character_Data is record
+      Texture           : GL.Objects.Textures.Texture;
+      Width             : GL.Types.Single;
+      Height            : GL.Types.Single;
+      Advance           : GL.Types.Single;
+      Ascend            : GL.Types.Single;
+      Descend           : GL.Types.Single;
+      Bearing_X         : GL.Types.Single;
+      Bearing_Y         : GL.Types.Single;
+      Valid             : Boolean := False;
+   end record;
+
+end FTGL_Interface;
