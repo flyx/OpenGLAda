d034cbb15a72bfb9f2d896443a62dc027e1d9d67
Roger <rogermc@grapevine.com.au>
2017-07-23 21:18:18 +1000

    Change a uniform name.

--- examples/ftgl/text_rendering/src/shaders/text_fragment_shader.glsl
+++ examples/ftgl/text_rendering/src/shaders/text_fragment_shader.glsl
@@ -3,11 +3,11 @@
 in vec2 texture_coords;
 out vec4 colour;
 
-uniform sampler2D texture_sampler;
-uniform vec3 texture_colour;
+uniform sampler2D text_sampler;  //   Mono-colored bitmap image
+uniform vec3 text_colour;
 
 void main()
 {
-	vec4 sampled = vec4 (1.0, 1.0, 1.0, texture(texture_sampler, texture_coords).r);
-    colour = vec4(texture_colour, 1.0) * sampled;
+	vec4 sampled = vec4 (1.0, 1.0, 1.0, texture(text_sampler, texture_coords).r);
+    colour = vec4(text_colour, 1.0) * sampled;
 }
