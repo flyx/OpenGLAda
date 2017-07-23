eb178be1d7da434ead3e5d6e780f381bfca40976
Roger <rogermc@grapevine.com.au>
2017-07-23 10:44:44 +1000

    Add Print_Array6 procedure.

--- examples/common/utilities.ads
+++ examples/common/utilities.ads
@@ -14,7 +14,9 @@ package Utilities is
     procedure Clear_Background_Colour (Colour : GL.Types.Colors.Color);
     procedure Clear_Background_Colour_And_Depth (Colour : GL.Types.Colors.Color);
     procedure Enable_Mouse_Callbacks (Window : in out Glfw.Windows.Window; Enable : Boolean);
-    procedure Load_UV_Buffer is new
+    procedure Load_Element_Buffer is new
+      GL.Objects.Buffers.Load_To_Buffer (GL.Types.Int_Pointers);
+    procedure Load_Vertex_Buffer is new
       GL.Objects.Buffers.Load_To_Buffer (GL.Types.Singles.Vector2_Pointers);
     procedure Load_Vertex_Buffer is new
       GL.Objects.Buffers.Load_To_Buffer (GL.Types.Singles.Vector3_Pointers);
@@ -22,8 +24,12 @@ package Utilities is
       GL.Objects.Buffers.Load_To_Buffer (GL.Types.Singles.Vector4_Pointers);
     procedure Load_Vector5_Buffer is new GL.Objects.Buffers.Load_To_Buffer
       (Maths.Vector5_Pointers);
+    procedure Load_Vector6_Buffer is new GL.Objects.Buffers.Load_To_Buffer
+      (Maths.Vector6_Pointers);
+    procedure Print_GL_Int_Array (Name : String; anArray : GL.Types.Int_Array);
     procedure Print_GL_Array3 (Name : String; anArray : GL.Types.Singles.Vector3_Array);
     procedure Print_GL_Array4 (Name : String; anArray : GL.Types.Singles.Vector4_Array);
+    procedure Print_Array6 (Name : String; anArray : Maths.Vector6_Array);
     procedure Print_Matrix (Name : String; aMatrix : GL.Types.Singles.Matrix3);
     procedure Print_Matrix (Name : String; aMatrix : GL.Types.Singles.Matrix4);
     procedure Print_Vector (Name : String; aVector : GL.Types.Singles.Vector2);
