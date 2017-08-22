10d6d5ba3f32d5b21aa282ad34d511e7e3f2a387
Roger <rogermc@grapevine.com.au>
2017-08-22 17:27:00 +1000

    File name change to avoid naming conflicts.

--- examples/free_type/ft_glyphs-api.ads
+++ examples/free_type/ft_glyphs-api.ads
@@ -1,10 +1,15 @@
 
 with System;
 
+with FT_Glyph;
 with FT_Image;
 with FT_Types;
 
 package FT_Glyph.API is
+
+   procedure FT_Done_Glyph (Glyph : FT_Glyph.);
+   pragma Import (C, FT_Done_Glyph, "FT_Done_Glyph");
+
   function FT_Glyph_To_Bitmap
      (the_glyph   : System.Address;
       render_mode : FT_Types.FT_Render_Mode;
