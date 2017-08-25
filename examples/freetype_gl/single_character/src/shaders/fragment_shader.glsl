dd005f86f152a3d458d874f937e10a9189a00654
Roger <rogermc@grapevine.com.au>
2017-08-25 21:24:47 +1000

    Ongoing development.

--- examples/freetype_gl/single_character/src/shaders/fragment_shader.glsl
+++ examples/freetype_gl/single_character/src/shaders/fragment_shader.glsl
@@ -1,17 +1,19 @@
 #version 410
 
-uniform float fade_factor;
-uniform sampler2D textures[2];
+in vec2 texture_coords;
 
-in vec2 texcoord;
+out vec4 frag_colour;
 
-out vec4 Frag_Colour;
+uniform sampler2D bitmap_image;  //   Mono-colored bitmap image
+uniform vec3 text_colour;
 
 void main()
 {
-    vec2 flipped_texcoord = vec2(texcoord.x, 1.0 - texcoord.y);
-    Frag_Colour = mix(
+    vec4 sampled = vec4 (1.0, 1.0, 1.0, texture(bitmap_image, texture_coords).r);
+    frag_colour = vec4(text_colour, 1.0) * sampled;
+    // vec2 flipped_texcoord = vec2(texcoord.x, 1.0 - texcoord.y);
+    /* frag_colour = mix(
          texture(textures[0], flipped_texcoord),
          texture(textures[1], flipped_texcoord),
-         fade_factor);
+         fade_factor); */
 }
