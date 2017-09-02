75dd4290e60db1bb224087e5bdf0f27b9b73e956
Roger <rogermc@grapevine.com.au>
2017-09-02 21:46:26 +1000

    Add new example Render_Text

---
+++ examples/freetype_gl/text_rendering/src/main_loop.adb
@@ -0,0 +1,228 @@
+
+with Interfaces.C;
+with Interfaces.C.Strings;
+with System;
+
+with Ada.Exceptions; use Ada.Exceptions;
+with Ada.Text_IO; use Ada.Text_IO;
+
+with GL.Attributes;
+with GL.Blending;
+with GL.Objects.Buffers;
+with GL.Objects.Programs;
+with GL.Objects.Shaders;
+with GL.Objects.Textures;
+with GL.Objects.Textures.Targets;
+with GL.Objects.Vertex_Arrays;
+with GL.Pixels;
+with GL.Toggles;
+with GL.Types;
+with GL.Types.Colors;
+with GL.Uniforms;
+
+with Glfw.Input;
+with Glfw.Input.Keys;
+with Glfw.Windows.Context;
+
+with Maths;
+with Program_Loader;
+with Utilities;
+
+with FT.Glyphs;
+with FT.Interfac;
+with FT.Utilities;
+
+with Texture_Manager;
+
+procedure Main_Loop (Main_Window : in out Glfw.Windows.Window) is
+
+   Vertex_Array          : GL.Objects.Vertex_Arrays.Vertex_Array_Object;
+   Vertex_Buffer         : GL.Objects.Buffers.Buffer;
+   Render_Program        : GL.Objects.Programs.Program;
+   Texture_ID            : GL.Uniforms.Uniform;
+   Colour_ID             : GL.Uniforms.Uniform;
+   Projection_Matrix     : GL.Types.Singles.Matrix4;
+   Projection_Matrix_ID  : GL.Uniforms.Uniform;
+
+   Background      : constant GL.Types.Colors.Color := (0.4, 0.6, 0.6, 1.0);
+   Text_Colour     : constant GL.Types.Colors.Basic_Color := (0.2, 0.4, 0.0);
+
+   --  ------------------------------------------------------------------------
+
+    procedure Load_Vertex_Sub_Buffer is new
+      GL.Objects.Buffers.Set_Sub_Data (GL.Types.Singles.Vector4_Pointers);
+
+   procedure Render_The_Text (Text   : String; X, Y, Scale : GL.Types.Single;
+                              Colour : GL.Types.Colors.Basic_Color);
+
+   --  ------------------------------------------------------------------------
+
+   procedure Render is
+      use GL.Types;
+      use GL.Objects.Buffers;
+   begin
+      Utilities.Clear_Background_Colour_And_Depth (Background);
+      Render_The_Text ("Some sample text.", 25.0, 25.0, 1.0, Text_Colour);
+
+   exception
+      when  others =>
+         Put_Line ("An exception occurred in Render.");
+         raise;
+   end Render;
+
+   --  ------------------------------------------------------------------------
+
+   procedure Render_The_Text (Text   : String; X, Y, Scale : GL.Types.Single;
+                              Colour : GL.Types.Colors.Basic_Color) is
+      use GL.Objects.Buffers;
+      use GL.Objects.Textures.Targets;
+      use GL.Types.Colors;
+      use GL.Types;
+
+      Char          : Character;
+      Char_Data     : Texture_Manager.Character_Record;
+      X_Orig        : Single := X;
+      Y_Orig        : Single := Y;
+      X_Pos         : Single;
+      Y_Pos         : Single;
+      Width         : Single;
+      Height        : Single;
+      --  2D quad as two triangles requires 2 * 3 vertices of 4 floats
+      Vertex_Data   : Singles.Vector4_Array (1 .. 6);
+   begin
+      GL.Objects.Programs.Use_Program (Render_Program);
+      GL.Uniforms.Set_Single (Colour_ID, Colour (R), Colour (G), Colour (B));
+      GL.Objects.Textures.Set_Active_Unit (0);
+      GL.Uniforms.Set_Int (Texture_ID, 0);  --  Added
+
+      Vertex_Array.Bind;
+
+      for index in Text'Range loop
+         Char := Text (index);
+         Char_Data := Texture_Manager.Character_Data.Element (index);
+         X_Pos := X_Orig + Char_Data.Bearing.Left * Scale;
+         Y_Pos := Y_Orig - (Char_Data.Size.Rows - Char_Data.Bearing.Top) * Scale;
+         Width := Char_Data.Size.Width;
+         Height := Char_Data.Size.Rows;
+         Vertex_Data := ((X_Pos, Y_Pos + Height,         0.0, 0.0),
+                         (X_Pos, Y_Pos,                  0.0, 1.0),
+                         (X_Pos + Width, Y_Pos,          1.0, 1.0),
+                         (X_Pos, Y_Pos + Height,         0.0, 0.0),
+                         (X_Pos + Width, Y_Pos,          1.0, 1.0),
+                         (X_Pos + Width, Y_Pos + Height, 1.0, 0.0));
+
+         GL.Attributes.Enable_Vertex_Attrib_Array (0);  --  Added
+         Texture_2D.Bind (Char_Data.Texture);
+         Array_Buffer.Bind (Vertex_Buffer);
+         Load_Vertex_Sub_Buffer (Array_Buffer, 0, Vertex_Data);
+
+         GL.Objects.Vertex_Arrays.Draw_Arrays (Triangles, 0, 6);
+         GL.Attributes.Disable_Vertex_Attrib_Array (0);  --  Added
+         --  Bitshift by 6 to get value in pixels (2^6 = 64
+         --  (divide amount of 1/64th pixels by 64 to get amount of pixels))
+         X_Orig := X_Orig + Single (Char_Data.Advance_X) / 64.0 * Scale;
+--           Put_Line ("X origin: " & Single'Image (X_Orig));
+      end loop;
+
+   exception
+      when  others =>
+         Put_Line ("An exception occurred in Render_The_Text.");
+         raise;
+   end Render_The_Text;
+
+   --  ------------------------------------------------------------------------
+
+   procedure Setup  (Window  : in out Glfw.Windows.Window) is
+      use GL.Objects.Programs;
+      use GL.Objects.Shaders;
+      use GL.Types;
+      use Program_Loader;
+
+      Window_Width    : Glfw.Size;
+      Window_Height   : Glfw.Size;
+   begin
+--        FTGL_Interface.Setup_Font (Font_Bitmap, Texture_Map,
+--                                        "/System/Library/Fonts/Helvetica.dfont");
+--        FTGL_Interface.Setup_Font (Font_Pixmap, Pixmap_Font_Data,
+--                                        "/System/Library/Fonts/Helvetica.dfont");
+--        FTGL_Interface.Setup_Font (Font_Polygon, Polygon_Font_Data,
+--                                        "/System/Library/Fonts/Helvetica.dfont");
+--        FTGL_Interface.Setup_Font (Font_Buffer, Buffer_Font_Data,
+--                                        "/System/Library/Fonts/Helvetica.dfont");
+
+      Window.Get_Framebuffer_Size (Window_Width, Window_Height);
+
+      GL.Toggles.Enable (GL.Toggles.Cull_Face);
+      GL.Toggles.Enable (GL.Toggles.Blend);
+      GL.Blending.Set_Blend_Func (GL.Blending.Src_Alpha,
+                                  GL.Blending.One_Minus_Src_Alpha);
+
+      Render_Program := Program_From
+          ((Src ("src/shaders/text_vertex_shader.glsl", Vertex_Shader),
+           Src ("src/shaders/text_fragment_shader.glsl", Fragment_Shader)));
+      Use_Program (Render_Program);
+
+      Projection_Matrix_ID := GL.Objects.Programs.Uniform_Location
+          (Render_Program, "projection_matrix");
+      Texture_ID := GL.Objects.Programs.Uniform_Location
+          (Render_Program, "text_sampler");
+      Colour_ID := GL.Objects.Programs.Uniform_Location
+          (Render_Program, "text_colour");
+
+      Maths.Init_Orthographic_Transform (0.0, Single (Window_Width),
+                                         0.0, Single (Window_Height),
+                                         -100.0, 100.0, Projection_Matrix);
+      GL.Uniforms.Set_Single (Projection_Matrix_ID, Projection_Matrix);
+
+      Vertex_Buffer.Initialize_Id;
+      GL.Objects.Buffers.Array_Buffer.Bind (Vertex_Buffer);
+
+      Setup_Font;
+      Setup_Texture;
+
+      Vertex_Array.Initialize_Id;
+      Vertex_Array.Bind;
+
+      Vertex_Buffer.Initialize_Id;
+      GL.Objects.Buffers.Array_Buffer.Bind (Vertex_Buffer);
+      GL.Objects.Buffers.Allocate (GL.Objects.Buffers.Array_Buffer,
+                 Single'Size / 8 * 6 * 4,  GL.Objects.Buffers.Dynamic_Draw);
+
+      GL.Attributes.Enable_Vertex_Attrib_Array (0);
+      GL.Attributes.Set_Vertex_Attrib_Pointer (Index => 0, Count  => 4,
+                                               Kind => Single_Type, Stride => 4,
+                                               Offset => 0);
+   exception
+      when others =>
+         Put_Line ("An exception occurred in Setup.");
+         raise;
+   end Setup;
+
+   --  ------------------------------------------------------------------------
+
+   use Glfw.Input;
+   Running : Boolean := True;
+begin
+   Setup (Main_Window);
+   while Running loop
+      Render;
+      Glfw.Windows.Context.Swap_Buffers (Main_Window'Access);
+      Glfw.Input.Poll_Events;
+      Running := Running and then not
+          (Main_Window.Key_State (Glfw.Input.Keys.Escape) = Glfw.Input.Pressed);
+      Running := Running and then not Main_Window.Should_Close;
+   end loop;
+   Put_Line ("Finished.");
+
+   Vertex_Buffer.Delete_Id;
+   Vertex_Array.Delete_Id;
+   Render_Program.Delete_Id;
+exception
+--     when anError : FTGL.FTGL_Error =>
+--        Put_Line ("Main_Loop returned an FTGL error: ");
+--        raise;
+
+   when anError :  others =>
+      Put_Line ("An exception occurred in Main_Loop.");
+      raise;
+end Main_Loop;
---
+++ examples/freetype_gl/text_rendering/src/texture_manager.adb
@@ -0,0 +1,201 @@
+
+with System;
+
+with Interfaces.C;
+
+with Ada.Exceptions; use Ada.Exceptions;
+with Ada.Text_IO; use  Ada.Text_IO;
+
+with GL.Attributes;
+with GL.Objects.Textures.Targets;
+with GL.Pixels;
+with GL.Types.Colors;
+
+with FT;
+with FT.API;
+with FT.Glyphs;
+with FT.Image;
+with FT.Interfac;
+with FT.Utilities;
+
+with Utilities;
+
+package body Texture_Manager is
+
+   theLibrary    : FT.API.Library_Ptr;
+   Face_Ptr      : FT.API.Face_Ptr;
+   Vertex_Data   : Vertex_Array;
+
+   Image_Error : exception;
+
+   procedure Setup_Buffer (Vertex_Buffer : in out V_Buffer;
+                           X, Y, Scale   : GL.Types.Single);
+   procedure Setup_Font;
+   procedure Setup_Texture (aTexture : in out GL.Objects.Textures.Texture);
+
+   --  ------------------------------------------------------------------------
+
+   procedure Print_Character_Data (Char : Character;
+                                   Data : Character_Record) is
+      use GL.Types;
+   begin
+      Put_Line ("Character" & Char & " Data");
+      Put_Line ("Width: " & Single'Image (Data.Size.Width));
+      Put_Line ("Rows: " & Single'Image (Data.Size.Rows));
+      Put_Line ("Left: " & Single'Image (Data.Bearing.Left));
+      Put_Line ("Top: " & Single'Image (Data.Bearing.Top));
+      Put_Line ("Advance X: " & Int'Image (Data.Advance_X) & " bits");
+      New_Line;
+   end Print_Character_Data;
+
+   --  ------------------------------------------------------------------------
+
+   procedure Setup_Buffer (Vertex_Buffer : in out V_Buffer;
+                           X, Y, Scale   : GL.Types.Single) is
+      use GL.Objects.Buffers;
+      use GL.Objects.Textures.Targets;
+      use GL.Types;
+      Slot_Ptr    : FT.API.Glyph_Slot_Ptr := FT.Interfac.Glyph_Slot (Face_Ptr);
+      X_Pos       : Single := X;
+      Y_Pos       : Single := Y ;
+      Width       : Single := FT.Glyphs.Bitmap_Width (Slot_Ptr) * Scale;
+      Height      : Single := Single (FT.Glyphs.Bitmap_Rows (Slot_Ptr)) * Scale;
+      Num_Triangles : Int := 2;
+      Stride        : Int := 4;
+   begin
+      Vertex_Buffer.Initialize_Id;
+      Array_Buffer.Bind (Vertex_Buffer);
+      Vertex_Data := (
+                      (X_Pos, Y_Pos,                  0.0, 0.0),  --  Lower left
+                      (X_Pos + Width, Y_Pos,          1.0, 0.0),  --  Lower right
+                      (X_Pos, Y_Pos + Height,         0.0, 1.0),  --  Upper left
+
+                      (X_Pos, Y_Pos + Height,         0.0, 1.0),  --  Upper left
+                      (X_Pos + Width, Y_Pos + Height, 1.0, 1.0),  --  Upper Right
+                      (X_Pos + Width, Y_Pos,          1.0, 0.0)); --  Lower right
+
+      Utilities.Load_Vertex_Buffer (Array_Buffer, Vertex_Data, Static_Draw);
+      GL.Attributes.Set_Vertex_Attrib_Pointer (Index  => 0, Count  => Num_Triangles,
+                                               Kind   => GL.Types.Single_Type,
+                                               Stride => Stride, Offset => 0);
+   exception
+      when others =>
+         Put_Line ("An exceptiom occurred in Setup_Buffer.");
+         raise;
+   end Setup_Buffer;
+
+   --  ------------------------------------------------------------------------
+
+   procedure Setup_Font is
+      use GL.Types;
+      --        Font_File       : String := "/Library/Fonts/Arial.ttf";
+      Font_File       : String := "/System/Library/Fonts/Helvetica.dfont";
+   begin
+      if FT.Interfac.New_Face (theLibrary, Font_File, 0, Face_Ptr) /= 0 then
+         Put_Line ("A face failed to load.");
+         raise FT.FT_Exception;
+      end if;
+      --  Set pixel size to 48 x 48
+      if FT.Interfac.Set_Pixel_Sizes (Face_Ptr, 0, 48) /= 0 then
+         Put_Line ("Unable to set pixel sizes.");
+         raise FT.FT_Exception;
+      end if;
+
+      GL.Pixels.Set_Unpack_Alignment (GL.Pixels.Bytes);  --  Disable byte-alignment restriction
+   exception
+      when others =>
+         Put_Line ("An exception occurred in Setup_Font.");
+         raise;
+   end Setup_Font;
+
+   --  ------------------------------------------------------------------------
+
+   procedure Setup_Graphic (Vertex_Buffer : in out V_Buffer;
+                            aTexture      : in out GL.Objects.Textures.Texture;
+                            X, Y: GL.Types.Single; Scale : GL.Types.Single;
+                            Char          : Character := 'g') is
+      use GL.Types;
+   begin
+      if FT.Interfac.Init_FreeType (theLibrary) /= 0 then
+         Put_Line ("The Freetype Library failed to load.");
+         raise FT.FT_Exception;
+      end if;
+
+      Setup_Font;
+      if FT.Interfac.Load_Character (Face_Ptr, Character'Pos (Char),
+                                      FT.Interfac.Load_Render) /= 0 then
+         Put_Line ("A character failed to load.");
+         raise FT.FT_Exception;
+      end if;
+
+      --  Ensure that the glyph image is an anti-aliased bitmap
+      if FT.Interfac.Render_Glyph (Face_Ptr, FT.API.Render_Mode_Mono) /= 0 then
+         Put_Line ("A character failed to render.");
+         raise FT.FT_Exception;
+      end if;
+      FT.Utilities.Print_Character_Metadata (Face_Ptr, Char);
+
+      Setup_Buffer (Vertex_Buffer, X, Y, Scale);
+      Setup_Texture (aTexture);
+
+      FT.Interfac.Done_Face (Face_Ptr);
+      FT.Interfac.Done_Library (theLibrary);
+   end Setup_Graphic;
+
+   --  ------------------------------------------------------------------------
+
+   procedure Setup_Texture (aTexture : in out GL.Objects.Textures.Texture) is
+      use GL.Objects.Textures.Targets;
+      use GL.Pixels;
+      use GL.Types;
+      Slot_Ptr       : FT.API.Glyph_Slot_Ptr := FT.Interfac.Glyph_Slot (Face_Ptr);
+      Priority       : GL.Objects.Textures.Priority := 0.9;
+      Width          : Size;
+      Height         : Size;
+      Bitmap_Image   : GL.Objects.Textures.Image_Source;
+      Char_Data      : Character_Record;
+      Character_Data : Character_Data_Vector;
+      aGlyph         : FT.Glyphs.Glyph_Record := FT.Glyphs.Glyph (Face_Ptr);
+   begin
+      Width := Size (FT.Glyphs.Bitmap_Width (Slot_Ptr));
+      Height := Size (FT.Glyphs.Bitmap_Rows (Slot_Ptr));
+
+      for Char in Interfaces.C.unsigned_long range 1 .. 128 loop
+         if FT.Interfac.Load_Character (Face_Ptr, Char, FT.Interfac.Load_Render) /= 0 then
+            Put_Line ("A character failed to load.");
+            raise FT.FT_Exception;
+         end if;
+
+         Char_Data.Size.Width := FT.Glyphs.Bitmap_Width (aGlyph);
+         Char_Data.Size.Rows := Single (FT.Glyphs.Bitmap_Rows (aGlyph));
+         Char_Data.Bearing.Left := Single (FT.Glyphs.Bitmap_Left (aGlyph));
+         Char_Data.Bearing.Top := Single (FT.Glyphs.Bitmap_Top (aGlyph));
+         Char_Data.Advance_X := FT.Image.Vector_X (FT.Glyphs.Glyph_Advance (aGlyph));
+         declare
+            aTexture   : GL.Objects.Textures.Texture;
+         begin
+            aTexture.Initialize_Id;
+            Texture_2D.Bind (aTexture);
+            Texture_2D.Set_Minifying_Filter (GL.Objects.Textures.Linear);
+            Texture_2D.Set_Magnifying_Filter (GL.Objects.Textures.Linear);
+            Texture_2D.Set_X_Wrapping (GL.Objects.Textures.Clamp_To_Edge); --  Wrap_S
+            Texture_2D.Set_Y_Wrapping (GL.Objects.Textures.Clamp_To_Edge); --  Wrap_T
+
+            Bitmap_Image := FT.Glyphs.Bitmap_Image (Slot_Ptr);
+            Texture_2D.Load_From_Data  (0, Red, Width, Height, Red,
+                                         Unsigned_Byte, Bitmap_Image);
+
+            Char_Data.Texture := aTexture;
+         end;
+
+         Character_Data.Append (Char_Data);
+      end loop;
+   exception
+      when others =>
+         Put_Line ("An exceptiom occurred in Setup_Texture.");
+         raise;
+   end Setup_Texture;
+
+   --  ------------------------------------------------------------------------
+
+end Texture_Manager;
