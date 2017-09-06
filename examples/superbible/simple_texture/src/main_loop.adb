9eee4e306c4a7a55436c999b44182529e5994292
Roger <rogermc@grapevine.com.au>
2017-09-06 18:37:42 +1000

    Ongoing development.

--- examples/superbible/simple_texture/src/main_loop.adb
+++ examples/superbible/simple_texture/src/main_loop.adb
@@ -4,17 +4,17 @@ with Interfaces;
 with Ada.Exceptions; use Ada.Exceptions;
 with Ada.Text_IO; use Ada.Text_IO;
 
+with GL;
 with GL.Buffers;
-with GL.Toggles;
+with GL.Errors;
 with GL.Objects.Buffers;
 with GL.Objects.Programs;
 with GL.Objects.Shaders;
 with GL.Objects.Textures;
 with GL.Objects.Textures.Targets;
 with GL.Objects.Textures.With_2D_Loader;
+with GL.Objects.Vertex_Arrays;
 with GL.Pixels;
-with GL.Rasterization;
-with GL.Toggles;
 with GL.Types;
 with GL.Types.Colors;
 
@@ -22,7 +22,6 @@ with Glfw.Input;
 with Glfw.Input.Keys;
 with Glfw.Windows.Context;
 
-with Maths;
 with Program_Loader;
 with Utilities;
 
@@ -32,21 +31,9 @@ procedure Main_Loop (Main_Window : in out Glfw.Windows.Window) is
    type Data is array (GL.Types.Int range <>) of GL.Types.Single;
 
     Rendering_Program  : GL.Objects.Programs.Program;
-    Vertex_Array       : GL.Objects.Vertex_Arrays;
+    Vertex_Array       : GL.Objects.Vertex_Arrays.Vertex_Array_Object;
     theTexture         : GL.Objects.Textures.Texture;
-
---  ------------------------------------------------------------------------
-
-   procedure Load_Subimage_2D   (Object : in out GL_Object;
-                  Level, X_Offset, Y_Offset : GL.Types.Int;
-                  Width, Height : GL.Types.Size;
-                  Format     : GL.Pixels.Data_Format; Data_Type : GL.Pixels.Data_Type;
-                  Image_Data : access GL.Objects.Textures.Image_Source) is
-   begin
-      GL_Load_Subimage_2D (Object, Level, X_Offset, Y_Offset,
-                           Width, Height, Format, Data_Type, Image_Data);
-      Raise_Exception_On_OpenGL_Error;
-   end Load_Subimage_2D;
+    Load_Image_Error   : Exception;
 
 --  ------------------------------------------------------------------------
 
@@ -77,11 +64,8 @@ procedure Main_Loop (Main_Window : in out Glfw.Windows.Window) is
 
     procedure Render is
         use GL.Types;
-        use Maths.Single_Math_Functions;
 
-        Back_Colour : GL.Types.Colors.Color :=
-          (0.5 * (1.0 + Sin (Single (';Current_Time))),
-           0.5 * (1.0 + Cos (Single (Current_Time))), 0.0, 1.0);
+        Back_Colour : GL.Types.Colors.Color := (0.0, 0.25, 0.0, 1.0);
     begin
         Utilities.Clear_Background_Colour_And_Depth (Back_Colour);
 
@@ -104,23 +88,27 @@ procedure Main_Loop (Main_Window : in out Glfw.Windows.Window) is
         Width        : constant Int := 256;
         Height       : constant Int := 256;
         MIP_Levels   : constant Int := 8;
-        Texture_Data : Data (1 .. Width * Height * 4);
+        X_Offset     : constant Int := 0;
+        Y_Offset     : constant Int := 0;
+        Texture_Data : aliased Data (1 .. Width * Height * 4);
+        Image_Data   : GL.Objects.Textures.Image_Source :=
+                       GL.Objects.Textures.Image_Source (Texture_Data'Address);
     begin
       theTexture.Initialize_Id;
       Texture_2D.Bind (theTexture);
       Texture_2D.Storage (MIP_Levels, GL.Pixels.RGBA32F, Width, Height);
       Generate_Texture (Texture_Data, Width, Height);
 
-        Rendering_Program := Program_From (
-          (Src ("src/shaders/st_vertex_shader.glsl", Vertex_Shader),
-           Src ("src/shaders/st_fragment_shader.glsl", Fragment_Shader))
-        );
-        GL.Toggles.Enable (gl.Toggles.Depth_Test);
-        GL.Buffers.Set_Depth_Function (gl.Types.Less);
+
+      Texture_2D.Load_SubImage_From_Data (0, X_Offset, Y_Offset,  Width, Height,
+                                          GL.Pixels.RGBA, GL.Pixels.Float,
+                                          Image_Data);
+        Rendering_Program := Program_From
+           ((Src ("src/shaders/st_vertex_shader.glsl", Vertex_Shader),
+             Src ("src/shaders/st_fragment_shader.glsl", Fragment_Shader)));
+
         Vertex_Array.Initialize_Id;
         Vertex_Array.Bind;
-        -- Point size is set in the vertex shader
-        gl.Toggles.Enable (gl.Toggles.Vertex_Program_Point_Size);
         Utilities.Show_Shader_Program_Data (Rendering_Program);
     exception
         when anError :  others =>
