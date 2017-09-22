f75278cd41f15dba00a29d0da5ec67c74f7b2bd6
Roger Mc Murtrie <rogermc@grapevine.com.au>
2017-09-22 23:33:35 +1000

    Updated fot conistency with texsub_example branch.

--- src/gl/interface/gl-objects-buffers.ads
+++ src/gl/interface/gl-objects-buffers.ads
@@ -55,9 +55,9 @@ package GL.Objects.Buffers is
 
    generic
       with package Pointers is new Interfaces.C.Pointers (<>);
-   procedure Set_Sub_Data (Target : Buffer_Target;
+   procedure Get_Sub_Data (Target : in out Buffer_Target;
                            Offset : Types.Size;
-                           Data   : Pointers.Element_Array);
+                           Data   : in out Pointers.Element_Array);
 
    function Access_Type (Target : Buffer_Target) return Access_Kind;
    function Mapped      (Target : Buffer_Target) return Boolean;
