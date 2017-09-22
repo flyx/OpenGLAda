532a8b738ad2484512156ce9178c4cc535ac7acb
Roger <rogermc@grapevine.com.au>
2017-09-22 19:10:26 +1000

    Initial changes to implement texture storage.

--- examples/freetype_gl/single_storage/src/texture_manager.adb
+++ examples/freetype_gl/single_storage/src/texture_manager.adb
@@ -1,6 +1,7 @@
 
 with System;
 
+with Ada.Containers.Vectors;
 with Ada.Exceptions; use Ada.Exceptions;
 with Ada.Text_IO; use  Ada.Text_IO;
 
@@ -21,20 +22,91 @@ with Utilities;
 
 package body Texture_Manager is
 
-   theLibrary    : FT.API.Library_Ptr;
-   Face_Ptr      : FT.API.Face_Ptr;
-   Vertex_Data   : Vertex_Array;
+   type Character_Size is record
+      Width     : GL.Types.Int;
+      Rows      : GL.Types.Int;
+   end record;
+
+   type Character_Bearing is record
+      Left      : GL.Types.Int;
+      Top       : GL.Types.Int;
+   end record;
+
+   type Character_Record is record
+      Texture   : GL.Objects.Textures.Texture;
+      Size      : Character_Size;
+      Bearing   : Character_Bearing;
+      Advance_X : GL.Types.Int;
+   end record;
+
+   package Data_Vector_Package is new
+     Ada.Containers.Vectors (Natural, Character_Record);
+   type Character_Data_Vector is new Data_Vector_Package.Vector with null record;
+
+   theLibrary     : FT.API.Library_Ptr;
+   Face_Ptr       : FT.API.Face_Ptr;
+   Character_Data : Character_Data_Vector;
+   Vertex_Data    : Vertex_Array;
 
    Image_Error : exception;
 
    procedure Setup_Buffer (Vertex_Buffer : in out V_Buffer;
+                           Char          : Character;
                            X, Y, Scale   : GL.Types.Single);
    procedure Setup_Font;
    procedure Setup_Texture (aTexture : in out GL.Objects.Textures.Texture);
 
    --  ------------------------------------------------------------------------
 
+   function Advance_X (Data : Character_Record) return GL.Types.Int is
+   begin
+      return Data.Advance_X;
+   end Advance_X;
+
+   --  ------------------------------------------------------------------------
+
+   function Data (Index : GL.Types.Int) return Character_Record is
+
+   begin
+      if Character_Data.Is_Empty then
+         raise Image_Error;
+      end if;
+      return Character_Data.Element (Natural (Index));
+   end Data;
+
+   --  ------------------------------------------------------------------------
+
+   function Left (Data : Character_Record) return GL.Types.Int is
+   begin
+      return Data.Bearing.Left;
+   end Left;
+
+   --  ------------------------------------------------------------------------
+
+   procedure Print_Character_Data (Char : Character;
+                                   Data : Character_Record) is
+      use GL.Types;
+   begin
+      Put_Line ("Character" & Char & " Data");
+      Put_Line ("Width: " & Int'Image (Data.Size.Width));
+      Put_Line ("Rows: " & Int'Image (Data.Size.Rows));
+      Put_Line ("Left: " & Int'Image (Data.Bearing.Left));
+      Put_Line ("Top: " & Int'Image (Data.Bearing.Top));
+      Put_Line ("Advance X: " & Int'Image (Data.Advance_X) & " bits");
+      New_Line;
+   end Print_Character_Data;
+
+   --  ------------------------------------------------------------------------
+
+   function Rows (Data : Character_Record) return GL.Types.Int is
+   begin
+      return Data.Size.Rows;
+   end Rows;
+
+   --  ------------------------------------------------------------------------
+
    procedure Setup_Buffer (Vertex_Buffer : in out V_Buffer;
+                           Char          : Character;
                            X, Y, Scale   : GL.Types.Single) is
       use GL.Objects.Buffers;
       use GL.Objects.Textures.Targets;
@@ -46,6 +118,18 @@ package body Texture_Manager is
       Num_Triangles : Int := 2;
       Stride        : Int := 4;
    begin
+     if FT.Interfac.Load_Character (Face_Ptr, Character'Pos (Char),
+                                      FT.Interfac.Load_Render) /= 0 then
+         Put_Line ("A character failed to load.");
+         raise FT.FT_Exception;
+     end if;
+
+      --  Ensure that the glyph image is an anti-aliased bitmap
+      if FT.Glyphs.Render_Glyph (Face_Ptr, FT.API.Render_Mode_Mono) /= 0 then
+         Put_Line ("A character failed to render.");
+         raise FT.FT_Exception;
+      end if;
+      FT.Utilities.Print_Character_Metadata (Face_Ptr, Char);
       Vertex_Buffer.Initialize_Id;
       Array_Buffer.Bind (Vertex_Buffer);
       Width := FT.Glyphs.Bitmap_Width (Face_Ptr) * Scale;
@@ -106,20 +190,7 @@ package body Texture_Manager is
       end if;
 
       Setup_Font;
-      if FT.Interfac.Load_Character (Face_Ptr, Character'Pos (Char),
-                                      FT.Interfac.Load_Render) /= 0 then
-         Put_Line ("A character failed to load.");
-         raise FT.FT_Exception;
-      end if;
-
-      --  Ensure that the glyph image is an anti-aliased bitmap
-      if FT.Glyphs.Render_Glyph (Face_Ptr, FT.API.Render_Mode_Mono) /= 0 then
-         Put_Line ("A character failed to render.");
-         raise FT.FT_Exception;
-      end if;
-      FT.Utilities.Print_Character_Metadata (Face_Ptr, Char);
-
-      Setup_Buffer (Vertex_Buffer, X, Y, Scale);
+      Setup_Buffer (Vertex_Buffer, Char, X, Y, Scale);
       Setup_Texture (aTexture);
 
       FT.Interfac.Done_Face (Face_Ptr);
@@ -134,28 +205,48 @@ package body Texture_Manager is
       use GL.Types;
       Width        : Size;
       Height       : Size;
+      X_Offset     : constant GL.Types.Int := 0;
+      Y_Offset     : constant GL.Types.Int := 0;
+      Char_Data    : Character_Record;
       Bitmap_Image : GL.Objects.Textures.Image_Source;
       Error_Code   : FT.FT_Error;
    begin
       Width := Size (FT.Glyphs.Bitmap_Width (Face_Ptr));
       Height := Size (FT.Glyphs.Bitmap_Rows (Face_Ptr));
 
+      Char_Data.Size.Width := Width;
+      Char_Data.Size.Rows := Height;
+      Char_Data.Bearing.Left := FT.Glyphs.Bitmap_Left (Face_Ptr);
+      Char_Data.Bearing.Top := FT.Glyphs.Bitmap_Top (Face_Ptr);
+      Char_Data.Advance_X := FT.Image.Vector_X (FT.Glyphs.Glyph_Advance (Face_Ptr));
+      Put_Line ("Setup_Textures, Width: " & GL.Types.Size'Image (Width));
+      Put_Line ("Setup_Textures, Height: " & GL.Types.Size'Image (Height));
       aTexture.Initialize_Id;
       Texture_2D.Bind (aTexture);
       Texture_2D.Set_Minifying_Filter (GL.Objects.Textures.Linear);
       Texture_2D.Set_Magnifying_Filter (GL.Objects.Textures.Linear);
       Texture_2D.Set_X_Wrapping (GL.Objects.Textures.Clamp_To_Edge); --  Wrap_S
       Texture_2D.Set_Y_Wrapping (GL.Objects.Textures.Clamp_To_Edge); --  Wrap_T
+
+      --  Mipmap level has to be set to 1 for Storage but
+      --  0 for Load_Sub_Image_From_Data!
+      Texture_2D.Storage (1, RGBA8, Width, Height);
+
       Error_Code := FT.Glyphs.Bitmap_Image (Face_Ptr, Bitmap_Image);
+      Put_Line ("Setup_Texture Bitmap_Image set, error code: " &
+                       FT.Errors.Error (Error_Code));
       if Error_Code = 0 then
-         Texture_2D.Load_From_Data  (0, Red, Width, Height, Red, Unsigned_Byte,
-                                     Bitmap_Image);
+         Texture_2D.Load_Sub_Image_From_Data     --  glTexSubImage2D
+              (0, X_Offset, Y_Offset, Width, Height, Red, Unsigned_Byte,
+               Bitmap_Image);
       else
-         Put_Line ("Setup_Texture error: " & FT.Errors.Error (Error_Code));
+         Put_Line ("Setup_Texture Bitmap_Image error: " &
+                       FT.Errors.Error (Error_Code));
+         raise FT.FT_Exception;
       end if;
    exception
       when others =>
-         Put_Line ("An exceptiom occurred in Setup_Texture.");
+         Put_Line ("An exception occurred in Setup_Texture.");
          raise;
    end Setup_Texture;
 
