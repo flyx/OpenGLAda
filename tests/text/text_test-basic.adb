156eb3f3e720755111c800e071c06ec847140855
Felix Krause <contact@flyx.org>
2017-10-26 18:00:42 +0200

    Renamed GL.FreeType to GL.Text

--- tests/text/text_test-basic.adb
+++ tests/text/text_test-basic.adb
@@ -7,19 +7,19 @@ with GL.Attributes;
 with GL.Blending;
 with GL.Buffers;
 with GL.Files;
-with GL.FreeType;
 with GL.Objects.Buffers;
 with GL.Objects.Shaders;
 with GL.Objects.Programs;
 with GL.Objects.Textures.Targets;
 with GL.Objects.Vertex_Arrays;
+with GL.Text;
 with GL.Toggles;
 with GL.Types.Colors;
 with GL.Uniforms;
 
 with GL_Test.Display_Backend;
 
-procedure FreeType_Test.Basic is
+procedure Text_Test.Basic is
    use GL.Buffers;
    use GL.Types;
    use GL.Objects.Vertex_Arrays;
@@ -56,9 +56,9 @@ procedure FreeType_Test.Basic is
       Program.Initialize_Id;
 
       GL.Files.Load_Shader_Source_From_File
-        (Vertex_Shader, "../tests/freetype/freetype_test-basic-vertex.glsl");
+        (Vertex_Shader, "../tests/text/text_test-basic-vertex.glsl");
       GL.Files.Load_Shader_Source_From_File
-        (Fragment_Shader, "../tests/freetype/freetype_test-basic-fragment.glsl");
+        (Fragment_Shader, "../tests/text/text_test-basic-fragment.glsl");
 
       Vertex_Shader.Compile;
       Fragment_Shader.Compile;
@@ -89,31 +89,31 @@ procedure FreeType_Test.Basic is
    Vector_Buffer1 : GL.Objects.Buffers.Buffer;
    Array1 : GL.Objects.Vertex_Arrays.Vertex_Array_Object;
 
-   Width, Y_Min, Y_Max : GL.FreeType.Pixel_Difference;
+   Width, Y_Min, Y_Max : GL.Text.Pixel_Difference;
    Text : constant String := "The quick brown fox jumps over the lazy dog.";
    Text_Image : GL.Objects.Textures.Texture;
-   Rendering_Program : GL.FreeType.Font_Rendering_Program;
-   Renderer : GL.FreeType.Renderer_Reference;
+   Rendering_Program : GL.Text.Font_Rendering_Program;
+   Renderer : GL.Text.Renderer_Reference;
    Texture_ID : GL.Uniforms.Uniform;
    Transformation : GL.Types.Singles.Matrix4;
    Transformation_ID : GL.Uniforms.Uniform;
    Dimensions_ID : GL.Uniforms.Uniform;
 
    use GL.Types.Singles;
-   use type GL.FreeType.Pixel_Difference;
+   use type GL.Text.Pixel_Difference;
 begin
    Display_Backend.Init;
    Display_Backend.Configure_Minimum_OpenGL_Version (Major => 3, Minor => 2);
    Display_Backend.Open_Window (Width => 500, Height => 500);
    Ada.Text_IO.Put_Line ("Initialized GLFW window");
 
-   Rendering_Program := GL.FreeType.Init_Program;
+   Rendering_Program := GL.Text.Init_Program;
    Renderer.Create (Rendering_Program,
                     "../tests/ftgl/SourceCodePro-Regular.ttf", 0);
    Renderer.Calculate_Dimensions (Text, Width, Y_Min, Y_Max);
 
    Ada.Text_IO.Put_Line ("Rendered text will have the dimensions" & Width'Img &
-     " x" & GL.FreeType.Pixel_Difference'Image (Y_Max - Y_Min) & ".");
+     " x" & GL.Text.Pixel_Difference'Image (Y_Max - Y_Min) & ".");
 
    Text_Image := Renderer.To_Texture
      (Text, Width, Y_Min, Y_Max, GL.Types.Colors.Color'(1.0, 0.0, 0.0, 1.0));
@@ -170,4 +170,4 @@ begin
    end loop;
 
    Display_Backend.Shutdown;
-end FreeType_Test.Basic;
+end Text_Test.Basic;
