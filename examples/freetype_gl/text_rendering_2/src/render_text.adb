7e80656fecdf6595fe937b7567cc691e150c4b18
Roger Mc Murtrie <rogermc@grapevine.com.au>
2017-10-29 00:43:35 +1100

    New example text_rendering_2.

---
+++ examples/freetype_gl/text_rendering_2/src/render_text.adb
@@ -0,0 +1,34 @@
+--  Program Render Text Example
+--  Author: Roger Mc Murtrie
+--  8 October 2017
+--  This example is based on Learn OpenGL>In Practice>Text Rendering at
+--  https://learnopengl.com/#!In-Practice/Text-Rendering
+
+with Ada.Exceptions; use Ada.Exceptions;
+with Ada.Text_IO; use Ada.Text_IO;
+
+with Glfw;
+with Glfw.Windows;
+
+with Initialize;
+with Main_Loop;
+
+procedure Render_Text is
+   Main_Window  : Glfw.Windows.Window;
+   Window_Title : constant String := "Render Text Example";
+begin
+   Glfw.Init;
+   Initialize (Main_Window, Window_Title);
+   Main_Loop (Main_Window);
+   Glfw.Shutdown;
+
+exception
+   when anError : Constraint_Error =>
+      Put_Line ("Render_Text returned a constraint error: ");
+      Put_Line (Exception_Information (anError));
+
+   when anError :  others =>
+      Put_Line ("An exception occurred in Render_Text.");
+      Put_Line (Exception_Information (anError));
+
+end Render_Text;
