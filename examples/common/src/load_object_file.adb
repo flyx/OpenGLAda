cb4dfc68da07975a54eec342ab12617245ab6445
Roger Mc Murtrie <rogermc@grapevine.com.au>
2017-11-26 23:21:10 +1100

    Ongoing development.

--- examples/common/src/load_object_file.adb
+++ examples/common/src/load_object_file.adb
@@ -5,37 +5,36 @@ with Ada.Strings.Unbounded;
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Text_IO.Unbounded_IO;
 
-with GL.Types;
---  with GL.Objects.Textures.Targets;
---  with GL.Pixels;
-
 package body Load_Object_File is
 
-   type V_String is array (GL.Types.Int range <>, GL.Types.Int range <>) of
+   type Vertex_String is array (GL.Types.Int range <>, GL.Types.Int range <>) of
      Ada.Strings.Unbounded.Unbounded_String;
+   type Vertex_Indices is array (GL.Types.Int range <>, GL.Types.Int range <>) of
+     GL.Types.Ints.Vector3;
    type Usemtl_Array is array (Integer range <>) of Ada.Strings.Unbounded.Unbounded_String;
 
    procedure Parse (F_String : Ada.Strings.Unbounded.Unbounded_String;
                     S_Index  : GL.Types.Int;
-                    V1_String, V2_String, V3_String : out V_String);
-    procedure Parse (Vertices_String : Ada.Strings.Unbounded.Unbounded_String;
-                     Vertices : out GL.Types.Singles.Vector2);
+                    V1_Indices, V2_Indices, V3_Indices : out Vertex_Indices);
+    procedure Parse (UVs_String : Ada.Strings.Unbounded.Unbounded_String;
+                     UVs : out GL.Types.Singles.Vector2);
     procedure Parse (Vertices_String : Ada.Strings.Unbounded.Unbounded_String;
                      Vertices : out GL.Types.Singles.Vector3);
 
    --  -------------------------------------------------------------------------
 
-   procedure Get_Num_Vertices (File_ID : Ada.Text_IO.File_Type;
-                               V_Count, VT_Count, VN_Count, S_Count : out GL.Types.Int;
-                               Usemtl_Count : out Integer; F_Count : out GL.Types.Int) is
+   procedure Get_Array_Sizes (File_ID : Ada.Text_IO.File_Type;
+                              Vertex_Count, UV_Count, Normal_Count,
+                              S_Count : out GL.Types.Int;
+                              Usemtl_Count : out Integer; F_Count : out GL.Types.Int) is
       use Ada.Strings.Unbounded;
       use GL.Types;
       Text  : Unbounded_String;
       Label : String (1 .. 2);
    begin
-      V_Count := 0;
-      VT_Count := 0;
-      VN_Count := 0;
+      Vertex_Count := 0;
+      UV_Count := 0;
+      Normal_Count := 0;
       S_Count := 0;
       Usemtl_Count := 0;
       F_Count := 0;
@@ -45,9 +44,9 @@ package body Load_Object_File is
          case Label (1) is
             when 'v' =>
                case Label (2) is
-                  when ' ' => V_Count := V_Count + 1;
-                  when 't' => VT_Count := VT_Count + 1;
-                  when 'n' => VN_Count := VN_Count + 1;
+                  when ' ' => Vertex_Count := Vertex_Count + 1;
+                  when 't' => UV_Count := UV_Count + 1;
+                  when 'n' => Normal_Count := Normal_Count + 1;
                   when others => null;
                end case;
             when 's' => S_Count := S_Count + 1;
@@ -56,24 +55,24 @@ package body Load_Object_File is
             when others => null;
          end case;
       end loop;
-   end Get_Num_Vertices;
+   end Get_Array_Sizes;
 
    --  -------------------------------------------------------------------------
 
    procedure Load_Data (File_ID  : Ada.Text_IO.File_Type;
                         Vertices : in out GL.Types.Singles.Vector3_Array;
-                        Verts    : in out GL.Types.Singles.Vector2_Array;
+                        UVs      : in out GL.Types.Singles.Vector2_Array;
                         Normals  : in out GL.Types.Singles.Vector3_Array;
-                        V1_String, V2_String, V3_String : in out V_String;
+                        V1_Indices, V2_Indices, V3_Indices : in out Vertex_Indices;
                         Usemtl   : in out Usemtl_Array) is
       use Ada.Strings.Unbounded;
       use GL.Types;
       Line         : Unbounded_String;
       Data         : Unbounded_String;
       Label        : String (1 .. 2);
-      V_Index      : Int := 0;
-      VT_Index     : Int := 0;
-      VN_Index     : Int := 0;
+      Vertex_Index : Int := 0;
+      UV_Index     : Int := 0;
+      Normal_Index : Int := 0;
       S_Index      : Int;
       F_String     : Unbounded_String;
       Usemtl_Index : Integer := 0;
@@ -86,19 +85,19 @@ package body Load_Object_File is
          case Label (1) is
             when 'v' =>
                case Label (2) is
-                  when ' ' => V_Index := V_Index + 1;
-                     Parse (Line, Vertices (V_Index));
-                  when 't' =>  VT_Index := VT_Index + 1;
-                     Parse (Line, Verts (VT_Index));
-                  when 'n' => VN_Index := VN_Index + 1;
-                     Parse (Line, Normals (VN_Index));
+                  when ' ' => Vertex_Index := Vertex_Index + 1;
+                     Parse (Line, Vertices (Vertex_Index));
+                  when 't' =>  UV_Index := UV_Index + 1;
+                     Parse (Line, UVs (UV_Index));
+                  when 'n' => Normal_Index := Normal_Index + 1;
+                     Parse (Line, Normals (Normal_Index));
                   when others => null;
                end case;
             when 's' =>  S_Index := Int'Value (To_String (Data));
             when 'u' => Usemtl_Index := Usemtl_Index + 1;
                Usemtl (Usemtl_Index) := Data;
             when 'f' => F_Index := F_Index + 1;
-               Parse (F_String, S_Index, V1_String, V2_String, V3_String);
+               Parse (F_String, S_Index, V1_Indices, V2_Indices, V3_Indices);
             when others => null;
          end case;
       end loop;
@@ -107,31 +106,32 @@ package body Load_Object_File is
    --  -------------------------------------------------------------------------
 
    procedure Load_Object (File_Name  : String;
-                          theTexture : out GL.Objects.Textures.Texture) is
+                          Vertices : out GL.Types.Singles.Vector3_Array;
+                          UVs      : out GL.Types.Singles.Vector2_Array;
+                          Normals  : out GL.Types.Singles.Vector3_Array) is
       Text_File_ID   : Ada.Text_IO.File_Type;
---        Stream_File_ID : Ada.Text_IO.Text_Streams.File_Type;
       Num_Vertices   : GL.Types.Int;
-      VT_Count       : GL.Types.Int;
-      VN_Count       : GL.Types.Int;
+      UV_Count       : GL.Types.Int;
+      Normal_Count   : GL.Types.Int;
       S_Count        : GL.Types.Int;
       Usemtl_Count   : Integer;
-      F_Count        : GL.Types.Int;
+      Triangle_Count : GL.Types.Int;
    begin
       Open (Text_File_ID, In_File, File_Name);
-      Get_Num_Vertices (Text_File_ID, Num_Vertices, VT_Count, VN_Count, S_Count,
-                        Usemtl_Count, F_Count);
+      Get_Array_Sizes (Text_File_ID, Num_Vertices, UV_Count, Normal_Count,
+                       S_Count, Usemtl_Count, Triangle_Count);
       declare
-         Vertices  : GL.Types.Singles.Vector3_Array (1 .. Num_Vertices);
-         Verts     : GL.Types.Singles.Vector2_Array (1 .. VT_Count);
-         Normals   : GL.Types.Singles.Vector3_Array (1 .. VN_Count);
-         V1_String : V_String (1 .. S_Count, 1 .. F_Count);
-         V2_String : V_String (1 .. S_Count, 1 .. F_Count);
-         V3_String : V_String (1 .. S_Count, 1 .. F_Count);
+--           Vertices  : GL.Types.Singles.Vector3_Array (1 .. Num_Vertices);
+--           UVs       : GL.Types.Singles.Vector2_Array (1 .. UV_Count);
+--           Normals   : GL.Types.Singles.Vector3_Array (1 .. Normal_Count);
+         V1_Indices : Vertex_Indices (1 .. S_Count, 1 .. Triangle_Count);
+         V2_Indices : Vertex_Indices (1 .. S_Count, 1 .. Triangle_Count);
+         V3_Indices : Vertex_Indices (1 .. S_Count, 1 .. Triangle_Count);
          Usemtl    : Usemtl_Array (1 .. Usemtl_Count);
       begin
          Reset (Text_File_ID);
-         Load_Data (Text_File_ID, Vertices, Verts, Normals,
-                    V1_String, V2_String, V3_String, Usemtl);
+         Load_Data (Text_File_ID, Vertices, UVs, Normals,
+                    V1_Indices, V2_Indices, V3_Indices, Usemtl);
          Close (Text_File_ID);
       end;
    exception
@@ -148,7 +148,16 @@ package body Load_Object_File is
 
    procedure Parse (F_String : Ada.Strings.Unbounded.Unbounded_String;
                     S_Index  : GL.Types.Int;
-                    V1_String, V2_String, V3_String : out V_String) is
+                    V1_Indices, V2_Indices, V3_Indices : out Vertex_Indices) is
+--        V1_String, V2_String, V3_String : Vertex_String;
+   begin
+      null;
+   end Parse;
+
+   --  -------------------------------------------------------------------------
+
+   procedure Parse (UVs_String : Ada.Strings.Unbounded.Unbounded_String;
+                    UVs : out GL.Types.Singles.Vector2) is
    begin
       null;
    end Parse;
@@ -156,15 +165,7 @@ package body Load_Object_File is
    --  -------------------------------------------------------------------------
 
    procedure Parse (Vertices_String : Ada.Strings.Unbounded.Unbounded_String;
-                     Vertices : out GL.Types.Singles.Vector2) is
-   begin
-      null;
-   end Parse;
-
-   --  -------------------------------------------------------------------------
-
-   procedure Parse (Vertices_String : Ada.Strings.Unbounded.Unbounded_String;
-                     Vertices : out GL.Types.Singles.Vector3) is
+                    Vertices : out GL.Types.Singles.Vector3) is
    begin
       null;
    end Parse;
--- examples/common/src/load_object_file.ads
+++ examples/common/src/load_object_file.ads
@@ -1,8 +1,10 @@
 
-with GL.Objects.Textures;
+with GL.Types;
 
 package Load_Object_File is
 
     procedure Load_Object (File_Name  : String;
-                        theTexture : out GL.Objects.Textures.Texture);
+                        Vertices : out GL.Types.Singles.Vector3_Array;
+                        UVs      : out GL.Types.Singles.Vector2_Array;
+                        Normals  : out GL.Types.Singles.Vector3_Array);
 end Load_Object_File;
