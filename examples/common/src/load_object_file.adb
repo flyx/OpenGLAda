ee6460f0944b8c1d502bc824dacce61b677a0fed
Roger Mc Murtrie <rogermc@grapevine.com.au>
2017-11-26 18:48:09 +1100

    Ongoing development.

--- examples/common/src/load_object_file.adb
+++ examples/common/src/load_object_file.adb
@@ -3,7 +3,7 @@
 with Ada.IO_Exceptions; use Ada.IO_Exceptions;
 with Ada.Strings.Unbounded;
 with Ada.Text_IO; use Ada.Text_IO;
-with Ada.Text_IO.Text_Streams; use Ada.Text_IO.Text_Streams;
+with Ada.Text_IO.Unbounded_IO;
 
 with GL.Types;
 --  with GL.Objects.Textures.Targets;
@@ -13,15 +13,21 @@ package body Load_Object_File is
 
    type V_String is array (GL.Types.Int range <>, GL.Types.Int range <>) of
      Ada.Strings.Unbounded.Unbounded_String;
+   type Usemtl_Array is array (Integer range <>) of Ada.Strings.Unbounded.Unbounded_String;
 
-    procedure Parse (F_String : Ada.Strings.Unbounded.Unbounded_String;
-                     V1_String, V2_String, V3_String : out V_String);
+   procedure Parse (F_String : Ada.Strings.Unbounded.Unbounded_String;
+                    S_Index  : GL.Types.Int;
+                    V1_String, V2_String, V3_String : out V_String);
+    procedure Parse (Vertices_String : Ada.Strings.Unbounded.Unbounded_String;
+                     Vertices : out GL.Types.Singles.Vector2);
+    procedure Parse (Vertices_String : Ada.Strings.Unbounded.Unbounded_String;
+                     Vertices : out GL.Types.Singles.Vector3);
 
    --  -------------------------------------------------------------------------
 
    procedure Get_Num_Vertices (File_ID : Ada.Text_IO.File_Type;
-                               V_Count, VT_Count, VN_Count, S_Count,
-                               Usemtl_Count, F_Count : out GL.Types.Int) is
+                               V_Count, VT_Count, VN_Count, S_Count : out GL.Types.Int;
+                               Usemtl_Count : out Integer; F_Count : out GL.Types.Int) is
       use Ada.Strings.Unbounded;
       use GL.Types;
       Text  : Unbounded_String;
@@ -58,37 +64,41 @@ package body Load_Object_File is
                         Vertices : in out GL.Types.Singles.Vector3_Array;
                         Verts    : in out GL.Types.Singles.Vector2_Array;
                         Normals  : in out GL.Types.Singles.Vector3_Array;
-                        V1_String, V2_String, V3_String : out V_String) is
+                        V1_String, V2_String, V3_String : in out V_String;
+                        Usemtl   : in out Usemtl_Array) is
       use Ada.Strings.Unbounded;
       use GL.Types;
-      Data_Stream  : constant Stream_Access := Stream (File_ID);
+      Line         : Unbounded_String;
+      Data         : Unbounded_String;
       Label        : String (1 .. 2);
       V_Index      : Int := 0;
       VT_Index     : Int := 0;
       VN_Index     : Int := 0;
-      S_Index      : Int := 1;
+      S_Index      : Int;
       F_String     : Unbounded_String;
---        Usemtl_Index : Int := 0;
+      Usemtl_Index : Integer := 0;
       F_Index      : Int := 0;
    begin
       while not End_Of_File (File_ID) loop
-         String'Read (Data_Stream, Label);
+         Ada.Text_IO.Unbounded_IO.Get_Line (File_ID, Line);
+         Label := To_String (Line) (1 .. 2);
+         Data := Unbounded_Slice (Line, 2, To_String (Line)'Length);
          case Label (1) is
             when 'v' =>
                case Label (2) is
                   when ' ' => V_Index := V_Index + 1;
-                     Singles.Vector3'Read (Data_Stream, Vertices (V_Index));
+                     Parse (Line, Vertices (V_Index));
                   when 't' =>  VT_Index := VT_Index + 1;
-                     Singles.Vector2'Read (Data_Stream, Verts (VT_Index));
+                     Parse (Line, Verts (VT_Index));
                   when 'n' => VN_Index := VN_Index + 1;
-                     Singles.Vector3'Read (Data_Stream, Normals (VN_Index));
+                     Parse (Line, Normals (VN_Index));
                   when others => null;
                end case;
-            when 's' => Int'Read (Data_Stream, S_Index);
---              when 'u' => Usemtl_Count := Usemtl_Count + 1;
+            when 's' =>  S_Index := Int'Value (To_String (Data));
+            when 'u' => Usemtl_Index := Usemtl_Index + 1;
+               Usemtl (Usemtl_Index) := Data;
             when 'f' => F_Index := F_Index + 1;
-               Unbounded_String'Read (Data_Stream, F_String);
-               Parse (F_String, V1_String, V2_String, V3_String);
+               Parse (F_String, S_Index, V1_String, V2_String, V3_String);
             when others => null;
          end case;
       end loop;
@@ -104,7 +114,7 @@ package body Load_Object_File is
       VT_Count       : GL.Types.Int;
       VN_Count       : GL.Types.Int;
       S_Count        : GL.Types.Int;
-      Usemtl_Count   : GL.Types.Int;
+      Usemtl_Count   : Integer;
       F_Count        : GL.Types.Int;
    begin
       Open (Text_File_ID, In_File, File_Name);
@@ -117,10 +127,11 @@ package body Load_Object_File is
          V1_String : V_String (1 .. S_Count, 1 .. F_Count);
          V2_String : V_String (1 .. S_Count, 1 .. F_Count);
          V3_String : V_String (1 .. S_Count, 1 .. F_Count);
+         Usemtl    : Usemtl_Array (1 .. Usemtl_Count);
       begin
          Reset (Text_File_ID);
          Load_Data (Text_File_ID, Vertices, Verts, Normals,
-                    V1_String, V2_String, V3_String);
+                    V1_String, V2_String, V3_String, Usemtl);
          Close (Text_File_ID);
       end;
    exception
@@ -136,7 +147,24 @@ package body Load_Object_File is
      --  -------------------------------------------------------------------------
 
    procedure Parse (F_String : Ada.Strings.Unbounded.Unbounded_String;
+                    S_Index  : GL.Types.Int;
                     V1_String, V2_String, V3_String : out V_String) is
+   begin
+      null;
+   end Parse;
+
+   --  -------------------------------------------------------------------------
+
+   procedure Parse (Vertices_String : Ada.Strings.Unbounded.Unbounded_String;
+                     Vertices : out GL.Types.Singles.Vector2) is
+   begin
+      null;
+   end Parse;
+
+   --  -------------------------------------------------------------------------
+
+   procedure Parse (Vertices_String : Ada.Strings.Unbounded.Unbounded_String;
+                     Vertices : out GL.Types.Singles.Vector3) is
    begin
       null;
    end Parse;
