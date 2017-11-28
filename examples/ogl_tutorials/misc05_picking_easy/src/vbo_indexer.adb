242c23d02944445b53de9f083de1a08a49c74ee2
Roger Mc Murtrie <rogermc@grapevine.com.au>
2017-11-28 23:55:41 +1100

    vbo_indexer ongoing development.

--- examples/ogl_tutorials/misc05_picking_easy/src/vbo_indexer.adb
+++ examples/ogl_tutorials/misc05_picking_easy/src/vbo_indexer.adb
@@ -8,71 +8,72 @@ package body VBO_Indexer is
 --           (Key_Type => tElement, Element_Type => tEnergy_Level_Basis_Set);
 --     type tBasis_Set_Map is new Basis_Set_Map.Map with null record;
 
-   type Packed_Vertex is record
-      Position : GL.Types.Singles.Vector3;
-      UV       : GL.Types.Singles.Vector2;
-      Normal   : GL.Types.Singles.Vector3;
-   end record;
+--     type Packed_Vertex is record
+--        Position : GL.Types.Singles.Vector3;
+--        UV       : GL.Types.Singles.Vector2;
+--        Normal   : GL.Types.Singles.Vector3;
+--     end record;
 
    function Is_Near (V1, V2 : GL.Types.Single) return Boolean;
 
    --  -------------------------------------------------------------------------
 
-  function Get_Similar_Index_Slow (Packed : Packed_Vertex) return Boolean is
-
+   procedure Get_Similar_Vertex_Index_Slow
+     (Vertex_In : GL.Types.Singles.Vector3;
+      UV_In     : GL.Types.Singles.Vector2;
+      Normal_In : GL.Types.Singles.Vector3;
+      Vertices  : GL.Types.Singles.Vector3_Array;
+      UVs       : GL.Types.Singles.Vector2_Array;
+      Normals   : GL.Types.Singles.Vector3_Array;
+      Result    : out GL.Types.Int; Found : out Boolean) is
    begin
-      return False;
-   end Get_Similar_Index_Slow;
+      Found := False;
+      for Index in Vertices'Range loop
+         Found := Is_Near (Vertex_In (GL.X), Vertices (Index) (GL.X))
+                  and then Is_Near (Vertex_In (GL.Y), Vertices (Index) (GL.Y))
+                  and then Is_Near (Vertex_In (GL.Z), Vertices (Index) (GL.Z))
+                  and then Is_Near (UV_In (GL.X), UVs (Index) (GL.X))
+                  and then Is_Near (UV_In (GL.Y), UVs (Index) (GL.Y))
+                  and then Is_Near (Normal_In (GL.X), Normals (Index) (GL.X))
+                  and then Is_Near (Normal_In (GL.Y), Normals (Index) (GL.Y))
+                  and then Is_Near (Normal_In (GL.Z), Normals (Index) (GL.Z));
+         if Found then
+            Result := Index;
+            exit;
+         end if;
+      end loop;
+   end Get_Similar_Vertex_Index_Slow;
 
    --  -------------------------------------------------------------------------
 
    procedure Index_VBO (Vertices_In : GL.Types.Singles.Vector3_Array;
                         UVs_In      : GL.Types.Singles.Vector2_Array;
                         Normals_In  : GL.Types.Singles.Vector3_Array;
-                        Vertices    : GL.Types.Singles.Vector3_Array;
-                        UVs         : GL.Types.Singles.Vector2_Array;
-                        Normals     : GL.Types.Singles.Vector3_Array) is
-      Packed : Packed_Vertex;
-      Found  : Boolean := False;
+                        Vertices    : in out GL.Types.Singles.Vector3_Array;
+                        UVs         : in out GL.Types.Singles.Vector2_Array;
+                        Normals     : in out GL.Types.Singles.Vector3_Array;
+                        Indices     : out GL.Types.Ints.Vector3_Array) is
+      use GL.Types;
+      Index          : Int;
+      Indices_Index  : Int := 0;
+      Out_Index      : Int := 0;
+      Found          : Boolean := False;
    begin
-      Found := Get_Similar_Index_Slow (Packed);
-   end Index_VBO;
-
-   --  -------------------------------------------------------------------------
-
-   procedure Get_Similar_Vertex_index
-     (Vertices_In : GL.Types.Singles.Vector3_Array;
-      UVs_In      : GL.Types.Singles.Vector2_Array;
-      Normals_In  : GL.Types.Singles.Vector3_Array;
-      Vertices    : out GL.Types.Singles.Vector3_Array;
-      UVs         : out GL.Types.Singles.Vector2_Array;
-      Normals     : out GL.Types.Singles.Vector3_Array;
-      Result      : out Integer; Found : out Boolean) is
-      Packed : Packed_Vertex;
-   begin
-      Found := False;
-      for Index in Vertices'Range loop
-         Found := Is_Near (Vertices_In (Index) (GL.X),
-                           Vertices (Index) (GL.X))
-                  and then Is_Near (Vertices_In (Index) (GL.Y),
-                           Vertices (Index) (GL.Y))
-                  and then Is_Near (Vertices_In (Index) (GL.Z),
-                                    Vertices (Index) (GL.Z))
-                  and then Is_Near (UVs_In (Index) (GL.X),
-                           UVs (Index) (GL.X))
-                  and then Is_Near (UVs_In (Index) (GL.Y),
-                                    UVs (Index) (GL.Y))
-                  and then Is_Near (Normals_In (Index) (GL.X),
-                            Normals (Index) (GL.X))
-                  and then Is_Near (Normals_In (Index) (GL.Y),
-                             Normals (Index) (GL.Y))
-                  and then Is_Near (Normals_In (Index) (GL.Z),
-                                    Normals (Index) (GL.Z));
+      for Vert in Vertices_In'Range loop
+         Get_Similar_Vertex_Index_Slow (Vertices_In (Vert), UVs_In (Vert), Normals_In (Vert),
+                                        Vertices, UVs, Normals, Index, Found);
+         Indices_Index := Indices_Index + 1;
          if Found then
-            exit;
+            Indices (Indices_Index) := (Index, Index, Index);
+         else
+            Out_Index := Out_Index + 1;
+            Vertices (Out_Index) := Vertices_In (Index);
+            UVs (Out_Index) := UVs (Index);
+            Normals (Out_Index) := Normals_In (Index);
+            Indices (Indices_Index) := (Out_Index, Out_Index, Out_Index);
          end if;
       end loop;
-   end Get_Similar_Vertex_index;
+   end Index_VBO;
 
    --  -------------------------------------------------------------------------
 
--- examples/ogl_tutorials/misc05_picking_easy/src/vbo_indexer.ads
+++ examples/ogl_tutorials/misc05_picking_easy/src/vbo_indexer.ads
@@ -6,8 +6,9 @@ package VBO_Indexer is
    procedure Index_VBO (Vertices_In : GL.Types.Singles.Vector3_Array;
                         UVs_In      : GL.Types.Singles.Vector2_Array;
                         Normals_In  : GL.Types.Singles.Vector3_Array;
-                        Vertices    : GL.Types.Singles.Vector3_Array;
-                        UVs         : GL.Types.Singles.Vector2_Array;
-                        Normals     : GL.Types.Singles.Vector3_Array);
+                        Vertices    : in out GL.Types.Singles.Vector3_Array;
+                        UVs         : in out GL.Types.Singles.Vector2_Array;
+                        Normals     : in out GL.Types.Singles.Vector3_Array;
+                        Indices     : out GL.Types.Ints.Vector3_Array);
 
 end VBO_Indexer;
