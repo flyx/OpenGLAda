1ccc4b5476ac7164826f256c884b889ab1efb534
Roger Mc Murtrie <rogermc@grapevine.com.au>
2017-12-07 19:47:01 +1100

    Original shaders restored.

--- examples/ogl_tutorials/09_vbo_indexing/src/shaders/standard_fragment_shader.glsl
+++ examples/ogl_tutorials/09_vbo_indexing/src/shaders/standard_fragment_shader.glsl
@@ -2,10 +2,10 @@
 
 // Interpolated values from the vertex shaders
 in vec2 UV;
-in vec3 Position_Worldspace;
-in vec3 Normal_Camera_Space;
-in vec3 Eye_Direction_Camera_Space;
-in vec3 Light_Direction_Camera_Space;
+in vec3 Position_worldspace;
+in vec3 Normal_cameraspace;
+in vec3 EyeDirection_cameraspace;
+in vec3 LightDirection_cameraspace;
 
 // Ouput data
 out vec3 color;
@@ -13,10 +13,10 @@ out vec3 color;
 // Values that stay constant for the whole mesh.
 uniform sampler2D myTextureSampler;
 uniform mat4 MV;
-uniform vec3 Light_Position_Worldspace;
+uniform vec3 LightPosition_worldspace;
 
-void main()
-    {
+void main(){
+
 	// Light emission properties
 	// You probably want to put them as uniforms
 	vec3 LightColor = vec3(1,1,1);
@@ -28,12 +28,12 @@ void main()
 	vec3 MaterialSpecularColor = vec3(0.3,0.3,0.3);
 
 	// Distance to the light
-	float distance = length( Light_Position_Worldspace - Position_Worldspace );
+	float distance = length( LightPosition_worldspace - Position_worldspace );
 
 	// Normal of the computed fragment, in camera space
-	vec3 n = normalize( Normal_Camera_Space );
+	vec3 n = normalize( Normal_cameraspace );
 	// Direction of the light (from the fragment to the light)
-	vec3 l = normalize( Light_Direction_Camera_Space );
+	vec3 l = normalize( LightDirection_cameraspace );
 	// Cosine of the angle between the normal and the light direction, 
 	// clamped above 0
 	//  - light is at the vertical of the triangle -> 1
@@ -42,7 +42,7 @@ void main()
 	float cosTheta = clamp( dot( n,l ), 0,1 );
 	
 	// Eye vector (towards the camera)
-	vec3 E = normalize(Eye_Direction_Camera_Space);
+	vec3 E = normalize(EyeDirection_cameraspace);
 	// Direction in which the triangle reflects the light
 	vec3 R = reflect(-l,n);
 	// Cosine of the angle between the Eye vector and the Reflect vector,
@@ -58,4 +58,5 @@ void main()
 		MaterialDiffuseColor * LightColor * LightPower * cosTheta / (distance*distance) +
 		// Specular : reflective highlight, like a mirror
 		MaterialSpecularColor * LightColor * LightPower * pow(cosAlpha,5) / (distance*distance);
-    }
+
+}
--- examples/ogl_tutorials/09_vbo_indexing/src/shaders/standard_vertex_shader.glsl
+++ examples/ogl_tutorials/09_vbo_indexing/src/shaders/standard_vertex_shader.glsl
@@ -1,44 +1,44 @@
 #version 410 core
 
 // Input vertex data, different for all executions of this shader.
-layout(location = 0) in vec3 Vertex_Position_Modelspace;
-layout(location = 1) in vec2 Vertex_UV;
-layout(location = 2) in vec3 Vertex_Normal_Modelspace;
+layout(location = 0) in vec3 vertexPosition_modelspace;
+layout(location = 1) in vec2 vertexUV;
+layout(location = 2) in vec3 vertexNormal_modelspace;
 
 // Output data ; will be interpolated for each fragment.
 out vec2 UV;
-out vec3 Position_Worldspace;
-out vec3 Normal_Camera_Space;
-out vec3 Eye_Direction_Camera_Space;
-out vec3 Light_Direction_Camera_Space;
+out vec3 Position_worldspace;
+out vec3 Normal_cameraspace;
+out vec3 EyeDirection_cameraspace;
+out vec3 LightDirection_cameraspace;
 
 // Values that stay constant for the whole mesh.
 uniform mat4 MVP;
 uniform mat4 V;
 uniform mat4 M;
-uniform vec3 Light_Position_Worldspace;
+uniform vec3 LightPosition_worldspace;
 
-void main()
-    {
+void main(){
+
 	// Output position of the vertex, in clip space : MVP * position
-	gl_Position =  MVP * vec4(Vertex_Position_Modelspace, 1);
+	gl_Position =  MVP * vec4(vertexPosition_modelspace,1);
 	
 	// Position of the vertex, in worldspace : M * position
-	Position_Worldspace = (M * vec4(Vertex_Position_Modelspace, 1)).xyz;
+	Position_worldspace = (M * vec4(vertexPosition_modelspace,1)).xyz;
 	
 	// Vector that goes from the vertex to the camera, in camera space.
 	// In camera space, the camera is at the origin (0,0,0).
-	vec3 Vertex_Position_Camera_Space = ( V * M * vec4(Vertex_Position_Modelspace,1)).xyz;
-	Eye_Direction_Camera_Space = vec3(0, 0, 0) - Vertex_Position_Camera_Space;
+	vec3 vertexPosition_cameraspace = ( V * M * vec4(vertexPosition_modelspace,1)).xyz;
+	EyeDirection_cameraspace = vec3(0,0,0) - vertexPosition_cameraspace;
 
 	// Vector that goes from the vertex to the light, in camera space. M is ommited because it's identity.
-	vec3 LightPosition_cameraspace = ( V * vec4(Light_Position_Worldspace, 1)).xyz;
-	Light_Direction_Camera_Space = Light_Direction_Camera_Space + Eye_Direction_Camera_Space;
+	vec3 LightPosition_cameraspace = ( V * vec4(LightPosition_worldspace,1)).xyz;
+	LightDirection_cameraspace = LightPosition_cameraspace + EyeDirection_cameraspace;
 	
 	// Normal of the the vertex, in camera space
-	Normal_Camera_Space = ( V * M * vec4(Vertex_Normal_Modelspace, 0)).xyz; // Only correct if ModelMatrix does not scale the model ! Use its inverse transpose if not.
+	Normal_cameraspace = ( V * M * vec4(vertexNormal_modelspace,0)).xyz; // Only correct if ModelMatrix does not scale the model ! Use its inverse transpose if not.
 	
 	// UV of the vertex. No special space for this one.
-	UV = Vertex_UV;
-    }
+	UV = vertexUV;
+}
 
