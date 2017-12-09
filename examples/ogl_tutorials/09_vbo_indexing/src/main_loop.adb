a5e6fff1d9257ddf5773998a1d85858424c9a499
Roger <rogermc@grapevine.com.au>
2017-12-09 14:25:48 +1100

    Buffer loading moved to separate routine Load_Buffers.

--- examples/ogl_tutorials/09_vbo_indexing/src/main_loop.adb
+++ examples/ogl_tutorials/09_vbo_indexing/src/main_loop.adb
@@ -49,139 +49,12 @@ procedure Main_Loop (Main_Window : in out Glfw.Windows.Window) is
 
    --  ------------------------------------------------------------------------
 
-   procedure Load_Texture (Window       : in out Glfw.Windows.Window;
-                           UV_Map       : GL.Objects.Textures.Texture) is
-      use GL.Types;
-      use GL.Types.Singles;
-      Model_Matrix      : constant Matrix4 := Identity4;
-      View_Matrix       : Matrix4;
-      Projection_Matrix : Matrix4;
-      MVP_Matrix        : Matrix4;
-      --        Light_Pos         : constant Vector3 := (4.0, 4.0, 4.0);
-   begin
-      Controls.Compute_Matrices_From_Inputs (Window, Projection_Matrix, View_Matrix);
-      Utilities.Clear_Background_Colour_And_Depth (White);
-      MVP_Matrix :=  Projection_Matrix * View_Matrix * Model_Matrix;
-
-      GL.Uniforms.Set_Single (Model_Matrix_ID, Model_Matrix);
-      GL.Uniforms.Set_Single (View_Matrix_ID, View_Matrix);
-      GL.Uniforms.Set_Single (MVP_Matrix_ID, MVP_Matrix);
-      GL.Uniforms.Set_Single (Light_Position_ID, 4.0, 4.0, 4.0);
-      --           GL.Uniforms.Set_Single (Light_Position_ID, Light_Pos);
-
-      --  Bind the texture in Texture Unit 0
-      GL.Objects.Textures.Set_Active_Unit (0);
-      GL.Objects.Textures.Targets.Texture_2D.Bind (UV_Map);
-      GL.Uniforms.Set_Int (Texture_ID, 0);
-
-   exception
-      when others =>
-         Put_Line ("An exception occurred in Load_Texture.");
-         raise;
-   end Load_Texture;
-
-   --  ------------------------------------------------------------------------
-
-   procedure Render (Window : in out Glfw.Windows.Window;
-                     Render_Program  : GL.Objects.Programs.Program;
-                     Indices_Size    : GL.Types.Int;
-                     UV_Map          : GL.Objects.Textures.Texture) is
-      use Interfaces.C;
+   Procedure Load_Buffers (Indices_Size : out GL.Types.Int) is
       use GL.Objects.Buffers;
       use GL.Types;
-      use Glfw.Input;
-      Current_Time : constant Glfw.Seconds := Glfw.Time;
-   begin
-      Number_Of_Frames := Number_Of_Frames + 1;
-      if Current_Time - Last_Time >= 1.0 then
-         Put_Line (Integer'Image (1000 * Number_Of_Frames) & " ms/frame");
-         Number_Of_Frames := 0;
-         Last_Time := Last_Time + 1.0;
-      end if;
-
-      Utilities.Clear_Colour_Buffer_And_Depth;
-      GL.Objects.Programs.Use_Program (Render_Program);
-      Load_Texture (Window, UV_Map);
-
-      --  First attribute buffer : vertices
-      GL.Attributes.Enable_Vertex_Attrib_Array (0);
-      GL.Objects.Buffers.Array_Buffer.Bind (Vertex_Buffer);
-      GL.Attributes.Set_Vertex_Attrib_Pointer (0, 3, Single_Type, 0, 0);
-      --  Second attribute buffer : UVs
-      GL.Attributes.Enable_Vertex_Attrib_Array (1);
-      GL.Objects.Buffers.Array_Buffer.Bind (UVs_Buffer);
-      GL.Attributes.Set_Vertex_Attrib_Pointer (1, 2, Single_Type, 0, 0);
-      --  Third attribute buffer : normals
-      GL.Attributes.Enable_Vertex_Attrib_Array (2);
-      GL.Objects.Buffers.Array_Buffer.Bind (Normals_Buffer);
-      GL.Attributes.Set_Vertex_Attrib_Pointer (2, 3, Single_Type, 0, 0);
-
-      --  Index Buffer
-      GL.Objects.Buffers.Element_Array_Buffer.Bind (Element_Buffer);
---        GL.Objects.Vertex_Arrays.Draw_Arrays (Triangles, 0,  Vertex_Count);
-      GL.Objects.Buffers.Draw_Elements (Triangles, Indices_Size, UInt_Type, 0);
-
-      GL.Attributes.Disable_Vertex_Attrib_Array (0);
-      GL.Attributes.Disable_Vertex_Attrib_Array (1);
-      GL.Attributes.Disable_Vertex_Attrib_Array (2);
-   exception
-      when others =>
-         Put_Line ("An exception occurred in Render.");
-         raise;
-   end Render;
-
-   --  ------------------------------------------------------------------------
-
-   procedure Setup (Window : in out Glfw.Windows.Window;
-                    Render_Program  : out GL.Objects.Programs.Program;
-                    Indices_Size    : out GL.Types.Int;
-                    UV_Map          : out GL.Objects.Textures.Texture) is
-      use GL.Objects.Buffers;
-      use GL.Objects.Shaders;
-      use GL.Objects.Textures.Targets;
-      use GL.Types;
-      use GL.Types.Singles;
-      use Glfw.Input;
-      Window_Width    : constant Glfw.Size := 1024;
-      Window_Height   : constant Glfw.Size := 768;
-      Vertices_Size   : Int;
       Vertex_Count    : Int;
-
+      Vertices_Size   : Int;
    begin
-      Window.Set_Input_Toggle (Sticky_Keys, True);
-      Window.Set_Cursor_Mode (Mouse.Disabled);
-      Glfw.Input.Poll_Events;
-
-      Window'Access.Set_Size (Window_Width, Window_Height);
-      Window'Access.Set_Cursor_Pos (Mouse.Coordinate (0.5 * Single (Window_Width)),
-                                    Mouse.Coordinate (0.5 * Single (Window_Height)));
-      Utilities.Clear_Background_Colour_And_Depth (Dark_Blue);
-
-      GL.Toggles.Enable (GL.Toggles.Depth_Test);
-      GL.Buffers.Set_Depth_Function (GL.Types.Less);
-      GL.Toggles.Enable (GL.Toggles.Cull_Face);
-
-      Vertices_Array_Object.Initialize_Id;
-      Vertices_Array_Object.Bind;
-
-      Render_Program := Program_Loader.Program_From
-        ((Program_Loader.Src ("src/shaders/standard_vertex_shader.glsl",
-         Vertex_Shader),
-         Program_Loader.Src ("src/shaders/standard_fragment_shader.glsl",
-           Fragment_Shader)));
-      Utilities.Show_Shader_Program_Data (Render_Program);
-
-      MVP_Matrix_ID := GL.Objects.Programs.Uniform_Location
-        (Render_Program, "MVP");
-      Model_Matrix_ID := GL.Objects.Programs.Uniform_Location
-        (Render_Program, "M");
-      View_Matrix_ID := GL.Objects.Programs.Uniform_Location
-        (Render_Program, "V");
-
-      Load_DDS ("src/textures/uvmap.DDS", UV_Map);
-      Texture_ID := GL.Objects.Programs.Uniform_Location
-        (Render_Program, "myTextureSampler");
-
       Vertex_Count := Load_Object_File.Mesh_Size ("src/textures/suzanne.obj");
       declare
          Vertices         : Singles.Vector3_Array (1 .. Vertex_Count);
@@ -223,8 +96,148 @@ procedure Main_Loop (Main_Window : in out Glfw.Windows.Window) is
             Element_Array_Buffer.Bind (Element_Buffer);
             Utilities.Load_Element_Buffer (Element_Array_Buffer, Indices, Static_Draw);
          end;
-
       end;
+
+   exception
+      when others =>
+         Put_Line ("An exception occurred in Load_Buffers.");
+         raise;
+   end Load_Buffers;
+
+   --  ------------------------------------------------------------------------
+
+   procedure Load_Matrices (Window  : in out Glfw.Windows.Window) is
+      use GL.Types;
+      use GL.Types.Singles;
+      Model_Matrix      : constant Matrix4 := Identity4;
+      View_Matrix       : Matrix4;
+      Projection_Matrix : Matrix4;
+      MVP_Matrix        : Matrix4;
+      --        Light_Pos         : constant Vector3 := (4.0, 4.0, 4.0);
+   begin
+      Controls.Compute_Matrices_From_Inputs (Window, Projection_Matrix, View_Matrix);
+      Utilities.Clear_Background_Colour_And_Depth (White);
+      MVP_Matrix :=  Projection_Matrix * View_Matrix * Model_Matrix;
+
+      GL.Uniforms.Set_Single (Model_Matrix_ID, Model_Matrix);
+      GL.Uniforms.Set_Single (View_Matrix_ID, View_Matrix);
+      GL.Uniforms.Set_Single (MVP_Matrix_ID, MVP_Matrix);
+      GL.Uniforms.Set_Single (Light_Position_ID, 4.0, 4.0, 4.0);
+      --           GL.Uniforms.Set_Single (Light_Position_ID, Light_Pos);
+
+   exception
+      when others =>
+         Put_Line ("An exception occurred in Load_Matrices.");
+         raise;
+   end Load_Matrices;
+
+   --  ------------------------------------------------------------------------
+
+   procedure Render (Window : in out Glfw.Windows.Window;
+                     Render_Program  : GL.Objects.Programs.Program;
+                     Indices_Size    : GL.Types.Int;
+                     UV_Map          : GL.Objects.Textures.Texture) is
+      use Interfaces.C;
+      use GL.Objects.Buffers;
+      use GL.Types;
+      use Glfw.Input;
+      Current_Time : constant Glfw.Seconds := Glfw.Time;
+   begin
+      Number_Of_Frames := Number_Of_Frames + 1;
+      if Current_Time - Last_Time >= 1.0 then
+         Put_Line (Integer'Image (1000 * Number_Of_Frames) & " ms/frame");
+         Number_Of_Frames := 0;
+         Last_Time := Last_Time + 1.0;
+      end if;
+
+      Utilities.Clear_Colour_Buffer_And_Depth;
+      GL.Objects.Programs.Use_Program (Render_Program);
+      Load_Matrices (Window);
+
+      --  First attribute buffer : vertices
+      GL.Attributes.Enable_Vertex_Attrib_Array (0);
+      GL.Objects.Buffers.Array_Buffer.Bind (Vertex_Buffer);
+      GL.Attributes.Set_Vertex_Attrib_Pointer (0, 3, Single_Type, 0, 0);
+      --  Second attribute buffer : UVs
+      GL.Attributes.Enable_Vertex_Attrib_Array (1);
+      GL.Objects.Buffers.Array_Buffer.Bind (UVs_Buffer);
+      GL.Attributes.Set_Vertex_Attrib_Pointer (1, 2, Single_Type, 0, 0);
+      --  Third attribute buffer : normals
+      GL.Attributes.Enable_Vertex_Attrib_Array (2);
+      GL.Objects.Buffers.Array_Buffer.Bind (Normals_Buffer);
+      GL.Attributes.Set_Vertex_Attrib_Pointer (2, 3, Single_Type, 0, 0);
+
+      --  Index Buffer
+      GL.Objects.Buffers.Element_Array_Buffer.Bind (Element_Buffer);
+
+      --  Bind the texture in Texture Unit 0
+      GL.Objects.Textures.Set_Active_Unit (0);
+      GL.Objects.Textures.Targets.Texture_2D.Bind (UV_Map);
+      GL.Uniforms.Set_Int (Texture_ID, 0);
+
+--        GL.Objects.Vertex_Arrays.Draw_Arrays (Triangles, 0, Indices_Size);
+      GL.Objects.Buffers.Draw_Elements (Triangles, Indices_Size, UInt_Type, 0);
+
+      GL.Attributes.Disable_Vertex_Attrib_Array (0);
+      GL.Attributes.Disable_Vertex_Attrib_Array (1);
+      GL.Attributes.Disable_Vertex_Attrib_Array (2);
+   exception
+      when others =>
+         Put_Line ("An exception occurred in Render.");
+         raise;
+   end Render;
+
+   --  ------------------------------------------------------------------------
+
+   procedure Setup (Window : in out Glfw.Windows.Window;
+                    Render_Program  : out GL.Objects.Programs.Program;
+                    Indices_Size    : out GL.Types.Int;
+                    UV_Map          : out GL.Objects.Textures.Texture) is
+      use GL.Objects.Buffers;
+      use GL.Objects.Shaders;
+      use GL.Objects.Textures.Targets;
+      use GL.Types;
+      use GL.Types.Singles;
+      use Glfw.Input;
+      Window_Width    : constant Glfw.Size := 1024;
+      Window_Height   : constant Glfw.Size := 768;
+
+   begin
+      Window.Set_Input_Toggle (Sticky_Keys, True);
+      Window.Set_Cursor_Mode (Mouse.Disabled);
+      Glfw.Input.Poll_Events;
+
+      Window'Access.Set_Size (Window_Width, Window_Height);
+      Window'Access.Set_Cursor_Pos (Mouse.Coordinate (0.5 * Single (Window_Width)),
+                                    Mouse.Coordinate (0.5 * Single (Window_Height)));
+      Utilities.Clear_Background_Colour_And_Depth (Dark_Blue);
+
+      GL.Toggles.Enable (GL.Toggles.Depth_Test);
+      GL.Buffers.Set_Depth_Function (GL.Types.Less);
+      GL.Toggles.Enable (GL.Toggles.Cull_Face);
+
+      Vertices_Array_Object.Initialize_Id;
+      Vertices_Array_Object.Bind;
+
+      Render_Program := Program_Loader.Program_From
+        ((Program_Loader.Src ("src/shaders/standard_vertex_shader.glsl",
+         Vertex_Shader),
+         Program_Loader.Src ("src/shaders/standard_fragment_shader.glsl",
+           Fragment_Shader)));
+      Utilities.Show_Shader_Program_Data (Render_Program);
+
+      MVP_Matrix_ID := GL.Objects.Programs.Uniform_Location
+        (Render_Program, "MVP");
+      Model_Matrix_ID := GL.Objects.Programs.Uniform_Location
+        (Render_Program, "M");
+      View_Matrix_ID := GL.Objects.Programs.Uniform_Location
+        (Render_Program, "V");
+
+      Load_DDS ("src/textures/uvmap.DDS", UV_Map);
+      Texture_ID := GL.Objects.Programs.Uniform_Location
+        (Render_Program, "myTextureSampler");
+
+      Load_Buffers (Indices_Size);
 
       Light_Position_ID := GL.Objects.Programs.Uniform_Location
         (Render_Program, "LightPosition_worldspace");
