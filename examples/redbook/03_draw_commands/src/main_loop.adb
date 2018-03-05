172194387ae6fff6970ea718237bb3e19d0dca33
Roger Mc Murtrie <rogermc@grapevine.com.au>
2018-03-05 22:39:03 +1100

    Initial completed version

--- examples/redbook/03_draw_commands/src/main_loop.adb
+++ examples/redbook/03_draw_commands/src/main_loop.adb
@@ -7,20 +7,24 @@ with GL.Objects.Buffers;
 with GL.Objects.Programs;
 with GL.Objects.Shaders;
 with GL.Objects.Vertex_Arrays;
+with GL.Toggles;
 with GL.Types;
 with GL.Types.Colors;
 with GL.Uniforms;
+with GL.Window;
 
 with Glfw;
 with Glfw.Input;
 with Glfw.Input.Keys;
 with Glfw.Windows.Context;
 
+with Maths;
 with Program_Loader;
 with Utilities;
 with Vertex_Data;
 
 procedure Main_Loop (Main_Window :  in out Glfw.Windows.Window) is
+   use GL.Types;
 
    Render_Program              : GL.Objects.Programs.Program;
    Vertex_Array                : GL.Objects.Vertex_Arrays.Vertex_Array_Object;
@@ -32,28 +36,64 @@ procedure Main_Loop (Main_Window :  in out Glfw.Windows.Window) is
 
    --  ------------------------------------------------------------------------
 
-   procedure Render is
-      use GL.Types;
+   procedure Render (Window :  in out Glfw.Windows.Window) is
       use GL.Objects.Buffers;
-      Dark_Blue : constant GL.Types.Colors.Color := (0.0, 0.0, 0.4, 1.0);
+      use GL.Objects.Vertex_Arrays;
+      use GL.Types.Singles;
+      Background        : constant GL.Types.Colors.Color := (0.7, 0.7, 0.7, 1.0);
       Window_Width      : Glfw.Size;
       Window_Height     : Glfw.Size;
       Aspect            : Single;
+      Model_Matrix      : Matrix4 := GL.Types.Singles.Identity4;
+      Projection_Matrix : Matrix4;
    begin
-      Utilities.Clear_Background_Colour (Dark_Blue);
+      Window.Get_Framebuffer_Size (Window_Width, Window_Height);
+      GL.Window.Set_Viewport (0, 0, GL.Types.Int (Window_Width),
+                              GL.Types.Int (Window_Height));
+      Aspect := Single (Window_Height) / Single (Window_Width);
+
+      GL.Toggles.Enable (GL.Toggles.Cull_Face);
+      Utilities.Clear_Background_Colour_And_Depth (Background);
+      --  Activate simple shading program
       GL.Objects.Programs.Use_Program (Render_Program);
 
-      GL.Attributes.Enable_Vertex_Attrib_Array (0);
-      Array_Buffer.Bind (Vertex_Buffer);
+      --   Set up the projection matrices
+      --  Top, Bottom, Left, Right, Near, Far
+      Maths.Init_Orthographic_Transform (-1.0, 1.0, -Aspect, Aspect, 1.0, 500.0,
+                                   Projection_Matrix);
+      Utilities.Print_Matrix ("Projection_Matrix", Projection_Matrix);
+      GL.Uniforms.Set_Single (Render_Projection_Matrix_ID, Projection_Matrix);
 
-      GL.Attributes.Set_Vertex_Attrib_Pointer (0, 3, Single_Type, 0, 0);
+      --  Set up for a Draw_Elements call
+      Vertex_Array.Bind;
+      Element_Array_Buffer.Bind (Element_Buffer);
 
-      GL.Objects.Vertex_Arrays.Draw_Arrays (Triangles, 0, 3);
-      GL.Attributes.Disable_Vertex_Attrib_Array (0);
+      --  Draw arrays
+      Model_Matrix := Model_Matrix * Maths.Translation_Matrix ((0.0, 0.0, 0.0));
+      Utilities.Print_Matrix ("Draw arrays Model_Matrix", Model_Matrix);
+      GL.Uniforms.Set_Single (Render_Model_Matrix_ID, Model_Matrix);
+      Draw_Arrays (Triangles, 0, 3);
+
+      -- Draw elements
+      Model_Matrix :=  Model_Matrix * Maths.Translation_Matrix ((-1.0, 0.0, -5.0));
+      GL.Uniforms.Set_Single (Render_Model_Matrix_ID, Model_Matrix);
+      Utilities.Print_Matrix ("Draw elements Model_Matrix", Model_Matrix);
+      Draw_Elements (Triangles, 3, UInt_Type);
+      Put_Line ("Main_Loop.Render, elements drawn.");
+
+      -- Draw elements base vertex
+      Model_Matrix :=  Maths.Translation_Matrix ((1.0, 0.0, -5.0));
+      GL.Uniforms.Set_Single (Render_Model_Matrix_ID, Model_Matrix);
+      Draw_Elements (Triangles, 3, UShort_Type);
+
+      --  Draw arrays instanced
+      Model_Matrix :=  Maths.Translation_Matrix ((3.0, 0.0, -5.0));
+      GL.Uniforms.Set_Single (Render_Model_Matrix_ID, Model_Matrix);
+      Draw_Arrays_Instanced (Triangles, 0, 3, 1);
 
    exception
       when  others =>
-         Put_Line ("An exceptiom occurred in Render.");
+         Put_Line ("An exceptiom occurred in Main_Loop.Render.");
          raise;
    end Render;
 
@@ -63,6 +103,8 @@ procedure Main_Loop (Main_Window :  in out Glfw.Windows.Window) is
       use GL.Objects.Buffers;
       use GL.Objects.Shaders;
       use Program_Loader;
+      Vertex_Data_Size : constant Int := Vertex_Data.Vertex_Positions'Size;
+      Colour_Data_Size : constant Int := Vertex_Data.Vertex_Colours'Size;
    begin
       Render_Program := Program_From
         ((Src ("src/shaders/primitive_restart_vs.glsl", Vertex_Shader),
@@ -74,23 +116,33 @@ procedure Main_Loop (Main_Window :  in out Glfw.Windows.Window) is
       Render_Projection_Matrix_ID := GL.Objects.Programs.Uniform_Location
         (Render_Program, "projection_matrix");
 
+      --  Set up the element array buffer
       Element_Buffer.Initialize_Id;
       Element_Array_Buffer.Bind (Element_Buffer);
       Utilities.Load_Element_Buffer (Element_Array_Buffer,
                                      Vertex_Data.Vertex_Indices,
                                      Static_Draw);
 
+      --  Set up the vertex attributes
       Vertex_Array.Initialize_Id;
       Vertex_Array.Bind;
 
       Vertex_Buffer.Initialize_Id;
       Array_Buffer.Bind (Vertex_Buffer);
-      Utilities.Load_Vertex_Buffer (Array_Buffer,
-                                    Vertex_Data.Vertex_Positions, Static_Draw);
+      Allocate (Array_Buffer, Long (Vertex_Data_Size + Colour_Data_Size), Static_Draw);
+      Utilities.Allocate_Vertex_Buffer
+        (Array_Buffer, 0, Vertex_Data.Vertex_Positions);
+      Utilities.Allocate_Vertex_Buffer
+        (Array_Buffer, Vertex_Data_Size, Vertex_Data.Vertex_Colours);
+
+      GL.Attributes.Set_Vertex_Attrib_Pointer (0, 4, Single_Type, 0, 0);
+      GL.Attributes.Set_Vertex_Attrib_Pointer (1, 4, Single_Type, 0, Vertex_Data_Size);
+      GL.Attributes.Enable_Vertex_Attrib_Array (0);
+      GL.Attributes.Enable_Vertex_Attrib_Array (1);
 
    exception
       when others =>
-         Put_Line ("An exceptiom occurred in Setup.");
+         Put_Line ("An exceptiom occurred in Main_Loop.Setup.");
          raise;
    end Setup;
 
@@ -101,7 +153,7 @@ procedure Main_Loop (Main_Window :  in out Glfw.Windows.Window) is
 begin
    Setup;
    while Running loop
-      Render;
+      Render (Main_Window);
       Glfw.Windows.Context.Swap_Buffers (Main_Window'Access);
       Glfw.Input.Poll_Events;
       Running := Running and not
