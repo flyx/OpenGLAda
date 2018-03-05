33d16c52ee9e8ca4842fba6b274cfa933d6d8865
Roger Mc Murtrie <rogermc@grapevine.com.au>
2018-03-05 22:36:24 +1100

    Draw_Arrays_Instanced implemented.

--- src/gl/generated/gl-api.ads
+++ src/gl/generated/gl-api.ads
@@ -466,6 +466,8 @@ private package GL.API is
    pragma Import (StdCall, Disable_Client_State, "glDisableClientState");
    procedure Draw_Arrays (Mode : Connection_Mode; First : Int; Count : Size);
    pragma Import (StdCall, Draw_Arrays, "glDrawArrays");
+   procedure Draw_Arrays_Instanced (Mode : Connection_Mode; First : Int; Count : Size; Instances : Size);
+   pragma Import (StdCall, Draw_Arrays_Instanced, "glDrawArraysInstanced");
    procedure Draw_Elements (Mode : Connection_Mode; Count : Size; Index_Type : Unsigned_Numeric_Type; Indices : Low_Level.IntPtr);
    pragma Import (StdCall, Draw_Elements, "glDrawElements");
    procedure Load_Matrix (Value : Types.Doubles.Matrix4);
