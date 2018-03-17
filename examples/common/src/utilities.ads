8b961229f37d9e41c1d11687e3a845a2f59e9f3a
Roger Mc Murtrie <rogermc@grapevine.com.au>
2018-03-17 14:36:57 +1100

    Restored wrongly deleted declarations

--- examples/common/src/utilities.ads
+++ examples/common/src/utilities.ads
@@ -10,6 +10,9 @@ with Maths;
 
 package Utilities is
 
+   type Byte_Array is array (GL.Types.Int range <>) of aliased GL.Types.UByte;
+   type Singles_Array is array (GL.Types.Int range <>) of aliased GL.Types.Single;
+
    procedure Clear_All (Colour : GL.Types.Colors.Color);
    procedure Clear_Background_Colour (Colour : GL.Types.Colors.Color);
    procedure Clear_Background_Colour_And_Depth (Colour : GL.Types.Colors.Color);
@@ -25,6 +28,10 @@ package Utilities is
      GL.Objects.Buffers.Load_To_Buffer (GL.Types.Singles.Vector3_Pointers);
    procedure Load_Vertex_Buffer is new
      GL.Objects.Buffers.Load_To_Buffer (GL.Types.Singles.Vector4_Pointers);
+   procedure Load_Texture_Buffer is new
+     GL.Objects.Buffers.Load_To_Texture_Buffer (GL.Types.Singles.Matrix4_Pointers);
+   procedure Load_Texture_Buffer is new
+     GL.Objects.Buffers.Load_To_Texture_Buffer (GL.Types.Singles.Vector4_Pointers);
    procedure Load_Vector5_Buffer is new GL.Objects.Buffers.Load_To_Buffer
      (Maths.Vector5_Pointers);
    procedure Load_Vector6_Buffer is new GL.Objects.Buffers.Load_To_Buffer
@@ -35,6 +42,8 @@ package Utilities is
      GL.Objects.Buffers.Set_Sub_Data (GL.Types.Singles.Vector4_Pointers);
 
    procedure Print_Array6 (Name : String; anArray : Maths.Vector6_Array);
+   procedure Print_Byte_Array (Name : String; anArray : Byte_Array;
+                               Start, Finish : GL.Types.UInt);
    procedure Print_GL_Array2 (Name : String; anArray : GL.Types.Ints.Vector2_Array);
    procedure Print_GL_Array2 (Name : String; anArray : GL.Types.Singles.Vector2_Array);
    procedure Print_GL_Array3 (Name : String; anArray : GL.Types.Ints.Vector3_Array);
@@ -44,6 +53,8 @@ package Utilities is
    procedure Print_GL_UInt_Array (Name : String; anArray : GL.Types.UInt_Array);
    procedure Print_Matrix (Name : String; aMatrix : GL.Types.Singles.Matrix3);
    procedure Print_Matrix (Name : String; aMatrix : GL.Types.Singles.Matrix4);
+   procedure Print_Singles_Array (Name : String; anArray : Singles_Array;
+                                  Start, Finish : GL.Types.Int);
    procedure Print_Vector (Name : String; aVector : GL.Types.Singles.Vector2);
    procedure Print_Vector (Name : String; aVector : GL.Types.Ints.Vector3);
    procedure Print_Vector (Name : String; aVector : GL.Types.Singles.Vector3);
