e0fe076dc3a53c0876a6ab4c4b9a369800a8477c
rogermc2 <rogermc@iinet.net.au>
2020-05-21 21:53:37 +1000

    Commented out call to Show_Shader_Program_Data which causes a gl exception

--- examples/ogl_tutorials/09_vbo_indexing/src/main_loop.adb
+++ examples/ogl_tutorials/09_vbo_indexing/src/main_loop.adb
@@ -220,7 +220,7 @@ procedure Main_Loop (Main_Window : in out Glfw.Windows.Window) is
          Vertex_Shader),
          Program_Loader.Src ("src/shaders/standard_fragment_shader.glsl",
            Fragment_Shader)));
-      Utilities.Show_Shader_Program_Data (Render_Program);
+--        Utilities.Show_Shader_Program_Data (Render_Program);
 
       MVP_Matrix_ID := GL.Objects.Programs.Uniform_Location
         (Render_Program, "MVP");
